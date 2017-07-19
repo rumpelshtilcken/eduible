062f7557c0375786942e780596442917af0f3236
ak8912 <kasaselya91@gmail.com>
2017-07-19 19:49:15 +0600

    feat: setup nodemon

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import next from 'next';
+const express = require('express');
+const next = require('next');
 // import router from './router';
 
 const dev = process.env.NODE_ENV !== 'production';
@@ -14,6 +14,7 @@ const runServer = async () => {
   server.get('*', (req, res) => handler(req, res));
 
   server.listen(3000, (err) => {
+    console.log('====');
     if (err) throw err;
     console.log('> Ready on http://localhost:3000');
   });
