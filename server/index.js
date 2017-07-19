6228f33e886525c5eec69c3599ef215754ae9a54
ak8912 <kasaselya91@gmail.com>
2017-07-19 19:32:51 +0600

    chore: update dependencies

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import next from 'next';
+// import router from './router';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handler = app.getRequestHandler();
+
+const runServer = async () => {
+  await app.prepare();
+  const server = express();
+
+  // server.use('/api/v1', router);
+  server.get('*', (req, res) => handler(req, res));
+
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+};
+
+runServer();
