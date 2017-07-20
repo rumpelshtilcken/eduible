9dae4c3e64bbe7ae37fa40310db6a521fd32779e
ak8912 <kasaselya91@gmail.com>
2017-07-20 20:56:09 +0600

    feat: add server side configs

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-import express from 'express';
-import next from 'next';
-import router from './router';
+const express = require('express');
+const next = require('next');
+// const comingsoon = require('../server/routes/comingsoon');
+// import router from './router';
+// const bodyParser = require('body-parser');
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const DEV = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 3000;
+
+const app = next({ dir: '.', dev: DEV });
 const handler = app.getRequestHandler();
 
 const runServer = async () => {
   await app.prepare();
   const server = express();
 
-  server.use('/api/v1', router);
+  // Load body parser to handle POST requests
+  //  server.use(bodyParser.json());
+  //  server.use(bodyParser.urlencoded({ extended: true }));
+  // server.use('/api/v1', router);
   server.get('*', (req, res) => handler(req, res));
-  server.get('/route/:id', (req, res) => app.render(req, res, '/routing', req.params));
-  server.all('*', (req, res) => handle(req, res));
-  server.listen(3000, (err) => {
+  // server.use('/comingSoon', comingsoon);
+
+  server.listen(PORT, (err) => {
+    console.log('====');
     if (err) throw err;
     console.log('> Ready on http://localhost:3000');
   });
