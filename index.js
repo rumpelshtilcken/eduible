14dc93df931615ab5ecfb06f16bfdf8b8c5ef97c
ak8912 <kasaselya91@gmail.com>
2017-07-20 21:01:15 +0600

    feat: add server side configs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const next = require('next');
-const comingsoon = require('../server/routes/comingsoon');
-import router from './router';
-const bodyParser = require('body-parser');
+// const comingsoon = require('../server/routes/comingsoon');
+// import router from './router';
+// const bodyParser = require('body-parser');
 
 const DEV = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 3000;
@@ -15,8 +15,8 @@ const runServer = async () => {
   const server = express();
 
   // Load body parser to handle POST requests
-  server.use(bodyParser.json());
-  server.use(bodyParser.urlencoded({ extended: true }));
+  // server.use(bodyParser.json());
+  // server.use(bodyParser.urlencoded({ extended: true }));
   // server.use('/api/v1', router);
   server.get('*', (req, res) => handler(req, res));
   //server.use('/comingSoon', comingsoon);
