2e7dca8934e59c56529143eee25e2931b61f31e5
White <apple@MacBook-Pro-apple-2.local>
2017-08-15 17:36:35 +0600

    feat: modal

---
+++ components/Header/HeaderModal/ModalDefault/index.js
@@ -0,0 +1,95 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import styles from './index.css';
+
+const config = [
+  {
+    title: 'FIRST AND LAST NAME',
+    input: {
+      type: 'string',
+      name: 'fullname',
+      className: 'input',
+      placeholder: 'John Smith'
+    }
+  },
+  {
+    title: 'DATE OF BIRTH',
+    input: {
+      type: 'string',
+      name: 'date',
+      className: 'input',
+      placeholder: '13/11/1992'
+    }
+  },
+  {
+    title: 'EMAIL',
+    input: {
+      type: 'string',
+      name: 'email',
+      className: 'input',
+      placeholder: 'example@email.com'
+    }
+  },
+  {
+    title: 'PASSWORD',
+    input: {
+      type: 'string',
+      name: 'email',
+      className: 'input',
+      placeholder: 'at least six characters'
+    }
+  }
+];
+
+class ModalDefault extends Component {
+  renderInput = item => (
+    <div>
+      <p>{item.title}</p>
+      <input
+        type={item.input.type}
+        name={item.input.name}
+        className={item.input.className}
+        placeholder={item.input.placeholder}
+      />
+      <style jsx>{styles}</style>
+    </div>
+  );
+
+  render() {
+    const { onOpenModal } = this.props;
+
+    return (
+      <div>
+        <h1 className="sign">SIGN UP</h1>
+        <div className="signUp">
+          <div className="inputBox">
+            {config.map(this.renderInput)}
+
+            <button className="continueButton" onClick={onOpenModal}>
+              CONTINUE
+            </button>
+          </div>
+          <div className="together">
+            <div className="inputBox2">
+              <p>OR SIGN UP USING</p>
+              <button className="facebookButton">FACEBOOK</button>
+              <button className="googleButton">GOOGLE</button>
+              <img alt="" id="line" src="static/Line123.jpg" />
+            </div>
+            <div className="inputBox3">
+              <p>ALREADY A MEMBER?</p>
+              <button className="loginButton">Log in here</button>
+            </div>
+          </div>
+        </div>
+        <style jsx>{styles}</style>
+      </div>
+    );
+  }
+}
+
+ModalDefault.propTypes = {
+  onOpenModal: PropTypes.func.isRequired
+};
+
+export default ModalDefault;
--- components/Header/index.js
+++ components/Header/index.js
@@ -1,78 +1,41 @@
-import Modal from 'react-modal';
 import { Component } from 'react';
 
-import fetch from 'isomorphic-fetch';
-import stylesheet from './index.css';
-import modalStylesheet from './modal.css';
+// import fetch from 'isomorphic-fetch';
+import HeaderModal from './HeaderModal';
+import stylesSheet from './index.css';
 
-const validator = require('email-validator');
+// const validator = require('email-validator');
 
 require('isomorphic-fetch');
 
-
 class Header extends Component {
-  constructor() {
-    super();
-    this.state = {
-      showModal: false,
-      email_input: '',
-      password_input: '',
-      confirmPassword_input: ''
-    };
-
-    this.handleOpenModal = this.handleOpenModal.bind(this);
-    this.handleCloseModal = this.handleCloseModal.bind(this);
-  }
-
-  handleOpenModal() {
-    this.setState({ showModal: true });
-  }
-
-  handleCloseModal() {
-    this.setState({ showModal: false });
-  }
-
-  handleClick = () => {
-    this.setState({
-      showModal: true,
-      email_input: '',
-      password_input: '',
-      confirmPassword_input: ''
-    });
+  state = {
+    isModalOpen: false
   };
+
+  handleRequestClose = () => this.setState({ isModalOpen: false });
+
+  handleSignUpButtonClik = () => console.log('handle') || this.setState({ isModalOpen: true });
+
   render() {
     return (
-      <header><div className="box">
-        <div className="logo">
-          <img src={'/static/eduible.svg'} alt={'logo'} />
-        </div>
-        <Modal
-          isOpen={this.state.showModal}
-          contentLabel="onRequestClose Example"
-          onRequestClose={this.handleCloseModal}
-          className="Modal"
-          overlayClassName="OverlayModal"
-        >
-          <div className="inputBox">
-            <h1>SIGN UP</h1>
-            <p>EMAIL</p>
-            <input ref={el => (this.state.email_input = el)} type="string" name="email" className="input" placeholder="john.doe@example.com" />
-            <p>PASSWORD</p>
-            <input ref={el => (this.state.password_input = el)} type="string" name="password" className="input" placeholder="at least 8 characters" />
-            <p>CONFIRM PASSWORD</p>
-            <input ref={el => (this.state.confirmPassword_input = el)} type="string" name="confrimPassword" className="input" />
-            <button className="continueButton">CONTINUE</button>
-            <p>OR SIGN UP USING</p>
-            <button className="facebookButton">FACEBOOK</button>
-            <button className="googleButton">GOOGLE</button>
-            <p>ALREADY A MEMBER</p>
-            <button className="loginButton">Log in here</button>
+      <header>
+        <div className="box">
+          <div className="logo">
+            <img src={'/static/eduible.svg'} alt={'logo'} />
           </div>
-        </Modal>
-        <button className="button" onClick={this.handleOpenModal}>SIGN UP</button>
-      </div>
-      <style jsx>{stylesheet}</style>
-      <style jsx global>{modalStylesheet}</style>
+          <HeaderModal
+            isModalOpen={this.state.isModalOpen}
+            className={stylesSheet.HeaderModal}
+            overlayClassName={stylesSheet.OverlayModal}
+            onRequestClose={this.handleRequestClose}
+          />
+
+          <button className="button" onClick={this.handleSignUpButtonClik}>
+            SIGN UP
+          </button>
+        </div>
+        <style jsx>{stylesSheet}</style>
       </header>
     );
   }
