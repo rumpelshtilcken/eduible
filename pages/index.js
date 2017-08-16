aee119a19091c5c787a8a580358aa8cd02465b69
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-16 12:57:53 +0600

    feat: introduce reduxStore

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
+import { Provider } from 'react-redux';
+
 import Page from '../components/Page';
 import Layout from '../components/Layout';
 import ComingSoon from '../components/ComingSoon';
 
+import getReduxStore from '../data/getReduxStore';
+
 class Homepage extends Page {
   render() {
     return (
-      <Layout title="Eduible" session={this.props.session}>
-        <ComingSoon />
-      </Layout>
+      <Provider store={getReduxStore({})}>
+        <Layout title="Eduible" session={this.props.session}>
+          <ComingSoon />
+        </Layout>
+      </Provider>
     );
   }
 }
