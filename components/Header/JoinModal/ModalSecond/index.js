2df2f5fba21c88bac509b27bb3fb36521a9e5a11
White <apple@MacBook-Pro-apple-2.local>
2017-08-17 12:49:45 +0600

    feat: modal second responsive

--- components/Header/JoinModal/ModalSecond/index.js
+++ components/Header/JoinModal/ModalSecond/index.js
@@ -6,25 +6,31 @@ const ModalSuccess = () => (
     <div className="firstTxt">
       <p className="share">Share your knowledge and experience. Start now - it’s free</p>
     </div>
-    <form className="codeForm">
-      <p>JOB TITLE</p>
-      <input
-        type="string"
-        name="fullname"
-        className="codeInput"
-        placeholder="IT developer"
-      />
-      <p className="company">COMPANY</p>
-      <input
-        type="string"
-        name="fullname"
-        className="codeInput"
-        placeholder="facebook"
-      />
-    </form>
-    <p className="provide">Providing your job title and company name  will help users find you on Eduible</p>
-    <button className="continueButton" onClick="openThird()">CONTINUE</button>
-    <a href="" className="ssylka">skip this step</a>
+    <div className="columns">
+      <div className="firstColumn">
+        <form className="codeForm">
+          <p>JOB TITLE</p>
+          <input
+            type="string"
+            name="fullname"
+            className="codeInput"
+            placeholder="IT developer"
+          />  
+          <p className="company">COMPANY</p>
+          <input
+            type="string"
+            name="fullname"
+            className="codeInput"
+            placeholder="facebook"
+          />
+        </form>
+      </div>
+      <div className="secondColumn">
+        <p className="provide">Providing your job title and company name  will help users find you on Eduible</p>
+        <button className="continueButton" onClick="openThird()">CONTINUE</button>
+        <a href="" className="ssylka">skip this step</a>
+      </div>
+    </div>
     <style jsx>{styles}</style>
   </div>
 );
