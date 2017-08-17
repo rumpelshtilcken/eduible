5ddf4763da27429ef64a45dd997784c3aaca2e0c
Ilyas Khametov <ilyas.xametov.1996@gmail.com>
2017-08-17 18:14:34 +0600

    feat(UserProfile): user profile info (top first) block with media

--- components/UserProfile/index.js
+++ components/UserProfile/index.js
@@ -5,7 +5,24 @@ class UserProfile extends Component {
   render() {
     return (
       <div className="container">
-        <div className="pageBlock pageBlock_halfRound" />
+        <div className="userProfileWrapper pageBlock pageBlock_halfRound">
+          <div className="userProfile">
+            <div className="userPhoto" />
+            <div className="userInfo">
+              <h2 className="userName">Anna Stark</h2>
+              <img
+                className="locationIcon"
+                src="/static/ic_location.svg"
+                alt="location"
+              />
+              <p className="userLocation">Miami, FL</p>
+            </div>
+          </div>
+          <div className="editIcon" />
+          <div className="editButton">
+            <button>Edit profile</button>
+          </div>
+        </div>
         <div className="pageBlockWrapper">
           <div className="tabMenu pageBlock" />
           <div className="universities pageBlock" />
