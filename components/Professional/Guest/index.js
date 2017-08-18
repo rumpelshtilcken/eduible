58dcf30c6fa54a05b6c7f3af15d82ce7233af19d
White <apple@MacBook-Pro-apple-2.local>
2017-08-18 15:34:54 +0600

    feat: guest-page

---
+++ components/Professional/Guest/index.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+import stylesSheet from './index.css';
+
+class Guest extends Component {
+  state = {
+
+  };
+  render() {
+    return (
+      <div className="container">
+        Hello White
+      </div>
+    );
+  }
+}
+
+export default Guest;
