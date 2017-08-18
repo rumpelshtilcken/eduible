df6773290bba7f5aef2594a125c84e998de6d7bc
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-18 19:43:16 +0600

    feat: introduce components

---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import CallRequest from './CallRequest';
+import CommingSoon from './ComingSoon';
+import Footer from './Footer';
+import Header from './Header';
+import Layout from './Layout';
+import LayoutComing from './LayoutComing';
+import Main from './Main';
+import Menu from './Menu';
+import Page from './Page';
+import PaymentDetails from './PaymentDetails';
+import SearchUniversity from './SearchUniversity';
+import Session from './Session';
+
+export {
+  CallRequest,
+  CommingSoon,
+  Footer,
+  Header,
+  Layout,
+  LayoutComing,
+  Main,
+  Menu,
+  Page,
+  PaymentDetails,
+  SearchUniversity,
+  Session
+};
