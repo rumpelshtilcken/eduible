b67001ab306803aea06856881e740571c730b979
Ilyas Khametov <ilyas.xametov.1996@gmail.com>
2017-08-18 17:45:48 +0600

    feat(StudentProfile): university card markup added (without course block)

--- components/StudentProfile/index.js
+++ components/StudentProfile/index.js
@@ -31,7 +31,25 @@ class StudentProfile extends Component {
               <span className="notifyDot" />
             </li>
           </ul>
-          <div className="universities pageBlock" />
+          <div className="universities pageBlock">
+            <div className="universityLogo" />
+            <a href="#" className="universityRemove">Remove from list</a>
+            <div className="universityWrapper">
+              <div className="universityInfo">
+                <h2 className="universityHeading universityTitle">Hogwarts</h2>
+                <p className="universityDescription">
+                  Hogwarts Castle is a large, seven-storey high building
+                  supported by magic, with a hundred and forty two staircases
+                  throughout its many towers and turrets and very deep dungeons.
+                </p>
+                <a href="#" className="readMore">Read more</a>
+              </div>
+              <div className="universityCourse">
+                <h2 className="universityHeading">Computer science</h2>
+                <a href="#" className="change">Change</a>
+              </div>
+            </div>
+          </div>
         </div>
         <style jsx>{stylesheet}</style>
       </div>
