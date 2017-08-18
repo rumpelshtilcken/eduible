504fefbc947086c9e1f62b9961eac5b0f597904e
Turan Nurtayev <psxi@MacBook-Pro-Turan.local>
2017-08-18 15:41:40 +0600

    feat: introduce videochat

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { Provider } from 'react-redux';
+
+import Page from '../components/Page';
+import Layout from '../components/Layout';
+import VideoChat from '../components/VideoChat';
+
+import getReduxStore from '../data/getReduxStore';
+
+class Homepage extends Page {
+  render() {
+    return (
+      <Provider store={getReduxStore({})}>
+        <Layout title="Eduible" session={this.props.session}>
+          <VideoChat />
+        </Layout>
+      </Provider>
+    );
+  }
+}
+
+export default Homepage;
