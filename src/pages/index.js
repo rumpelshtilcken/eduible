6bea772ac9c830a0492e9ba60693c39d5b92bfce
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-19 23:37:30 +0600

    feat(Pages): import components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import { Provider } from 'react-redux';
 
-import Page from 'components/Page';
-import Layout from 'components/Layout';
-import SearchUniversity from 'components/SearchUniversity';
-
+import { Page, Layout, CommingSoon } from 'components';
 import getReduxStore from 'data/getReduxStore';
 
 class Homepage extends Page {
@@ -11,7 +8,7 @@ class Homepage extends Page {
     return (
       <Provider store={getReduxStore({})}>
         <Layout title="Eduible" session={this.props.session}>
-          <SearchUniversity />
+          <CommingSoon />
         </Layout>
       </Provider>
     );
