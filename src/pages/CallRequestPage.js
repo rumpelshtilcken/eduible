529fd40864819e38e0df2f398a58f0ede9c052cc
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-19 23:35:08 +0600

    feat: introduce CallRequestPage

---
+++ src/pages/CallRequestPage.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+import CallRequestContainer from 'containers/CallRequestContainer';
+
+class CallRequestPage extends Component {
+  handleBackButtonClick = () => {
+    // TODO: navigate back
+  };
+
+  render() {
+    return (
+      <CallRequestContainer
+        onBackButtonClick={this.handleBackButtonClick}
+      />);
+  }
+}
+
+export default CallRequestPage;
