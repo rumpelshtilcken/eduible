45d8bca28911b2193f71ac003da8caeddb50a981
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-21 16:24:16 +0600

    feat(SelectDropdown): require key in options array

--- src/components/SelectDropdown/index.js
+++ src/components/SelectDropdown/index.js
@@ -2,29 +2,30 @@ import PropTypes from 'prop-types';
 
 import stylesheet from './index.css';
 
-const SelectDropdown = ({ key, onChange, options, value }) => (
-  <select key={key} onChange={onChange} value={value}>
+const SelectDropdown = ({ key, onChange, options, value }) =>
+  (<select key={key || value} onChange={onChange} value={value}>
     {options.map(opt =>
-      (<option
-        key={opt.value || opt.key}
-        value={opt.value}
-      >{opt.label}</option>))
-    }
+      (<option key={opt.value || opt.key} value={opt.value}>
+        {opt.label}
+      </option>)
+    )}
 
-    <style jsx>{stylesheet}</style>
-  </select>
-);
+    <style jsx>
+      {stylesheet}
+    </style>
+  </select>);
 
 SelectDropdown.propTypes = {
-  key: PropTypes.string,
+  key: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   onChange: PropTypes.func.isRequired,
   options: PropTypes.arrayOf(
     PropTypes.shape({
       key: PropTypes.string,
-      value: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired
-    }).isRequired).isRequired,
-  value: PropTypes.string.isRequired
+      value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      label: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired
+    }).isRequired
+  ).isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired
 };
 
 export default SelectDropdown;
