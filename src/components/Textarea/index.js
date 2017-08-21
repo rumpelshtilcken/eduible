b2ea729a714d8fcb4c380aacd3c47413fcffd6f2
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-21 17:01:07 +0600

    fix(Textarea): not require placeholder

--- src/components/Textarea/index.js
+++ src/components/Textarea/index.js
@@ -4,19 +4,20 @@ import stylessheet from './index.css';
 
 const Textarea = ({ placeholder, value, onChange }) =>
   (<div className="textareaContainer">
-    <textarea
-      className="textarea"
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder || 'Your Message'}
-    />
-    <style jsx>{stylessheet}</style>
+    <textarea className="textarea" value={value} onChange={onChange} placeholder={placeholder} />
+    <style jsx>
+      {stylessheet}
+    </style>
   </div>);
 
 Textarea.propTypes = {
-  placeholder: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired
+};
+
+Textarea.defaultProps = {
+  placeholder: 'Your Message'
 };
 
 export default Textarea;
