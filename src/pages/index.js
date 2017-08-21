c8701239cbd4ea09cff5a280d50b6bd8a6835497
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-21 16:57:10 +0600

    feat(Pages): review code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import { Provider } from 'react-redux';
 
-import { Page, Layout, CommingSoon } from 'components';
+import { Page, Layout } from 'components';
 import getReduxStore from 'data/getReduxStore';
+
+import ComingSoonPage from './ComingSoonPage';
 
 class Homepage extends Page {
   render() {
     return (
       <Provider store={getReduxStore({})}>
         <Layout title="Eduible" session={this.props.session}>
-          <CommingSoon />
+          <ComingSoonPage />
         </Layout>
       </Provider>
     );
