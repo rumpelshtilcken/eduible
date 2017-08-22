a66fb5eecac4aa12124cbf10db9fe7096b74b99d
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-22 16:57:49 +0600

    feat: introduce common types

---
+++ src/types/common.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+const UniversityType = PropTypes.shape({
+  logoUrl: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  rank: PropTypes.string,
+  facts: PropTypes.arrayOf(PropTypes.string)
+}).isRequired;
+
+export { UniversityType };
