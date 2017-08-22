5e0ff2abd04982a3afe67e6b28af5d3d575cd74b
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-22 16:54:14 +0600

    feat(Footer): add prop

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,37 @@
+import PropTypes from 'prop-types';
+
+import { Link } from 'components';
+
 import stylesheet from './index.css';
 
-const Footer = () => (
-  <header>
-    <div className="container">
-      <div className="leftSide">
-        <button className="menu">EXTRA MENU</button>
-        <button className="menu">EXTRA MENU</button>
-      </div>
-      <div className="copyright">COPYRIGHT (C) 2017</div>
+const Footer = ({ urlsMenu, copyright }) =>
+  (<div className="footerContainer">
+    <div className="menuLinks">
+      {urlsMenu.map(urlMenu =>
+        (<Link key={urlMenu.url} href={urlMenu.url}>
+          <a className="menu">
+            {urlMenu.title}
+          </a>
+        </Link>)
+      )}
     </div>
-    <style jsx>{stylesheet}</style>
-  </header>
-);
+
+    <div className="copyright">
+      {copyright}
+    </div>
+    <style jsx>
+      {stylesheet}
+    </style>
+  </div>);
+
+Footer.propTypes = {
+  urlsMenu: PropTypes.arrayOf(
+    PropTypes.shape({
+      url: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired
+    })
+  ).isRequired,
+  copyright: PropTypes.string.isRequired
+};
 
 export default Footer;
