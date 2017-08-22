1447d1e4ceac072fc2080b811f49ab3c892548d5
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-22 16:56:05 +0600

    feat(components): export new Components

--- app/components/index.js
+++ app/components/index.js
@@ -7,6 +7,7 @@ import FoundUniversities from './SearchUniversity/FoundUniversities';
 import Header from './Header';
 import InputRange from '../components/InputRange';
 import Layout from './Layout';
+import LayoutComing from './LayoutComing';
 import Link from './Link';
 import Main from './Main';
 import Menu from './Menu';
@@ -35,6 +36,7 @@ export {
   Header,
   InputRange,
   Layout,
+  LayoutComing,
   Link,
   Main,
   Menu,
