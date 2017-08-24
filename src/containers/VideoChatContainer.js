1c2391d720eb3d4f39ecd10612c64f5c31515576
Turan Nurtayev <psxi@MacBook-Pro-Turan.local>
2017-08-24 16:25:06 +0600

    feat: videochat refactoring

---
+++ src/containers/VideoChatContainer.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+import { VideoChat } from 'components';
+
+class VideoChatContainer extends Component {
+  render() {
+    return (
+      <VideoChat />
+    );
+  }
+}
+
+export default VideoChatContainer;
---
+++ src/pages/VideoChatPage.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+import VideoChatContainer from 'containers/VideoChatContainer';
+
+class VideoChatPage extends Component {
+  render() {
+    return (
+      <VideoChatContainer />
+    );
+  }
+}
+
+export default VideoChatPage;
