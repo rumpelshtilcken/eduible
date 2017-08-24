22c4ff539ac7cf5f95982aedf93c3622c94ac951
Turan Nurtayev <psxi@MacBook-Pro-Turan.local>
2017-08-24 16:25:06 +0600

    feat: videochat refactoring

--- components/VideoChat/index.js
+++ components/VideoChat/index.js
@@ -1,103 +1,15 @@
-import { Component } from 'react';
+import VideoBox from './VideoBox';
+import ChatBox from './ChatBox';
+import ProfileBox from './ProfileBox';
 import stylesheet from './index.css';
 
-class VideoChat extends Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="section1" />
-        <div className="section2">
-          <div>
-            <img id="profileImg" src="/static/miguel.jpg" alt="profileImg" />
-          </div>
-          <div id="profileDetails">
-            <p id="name">MIGUEL CARRERA</p>
-            <p><img src="/static/placeholderIconMobile.svg" alt="" /> <span id="smp">MIAMI, FL</span></p>
-          </div>
-          <div>
-            <p id="smn">0:59</p>
-          </div>
-        </div>
-        <div className="section3">
-          <div id="fo">
-            <div>
-              <input id="fo1" type="text" placeholder="I can't hear you!" />
-            </div>
-            <div>
-              <img id="fo2" src="/static/sendButtonMobile.svg" alt="" />
-            </div>
-          </div>
-          <div id="chatbox">
-            <div className="dialog">
-              <div className="smp1">
-                <h4>You</h4>
-              </div>
-              <div className="smp2">
-                <p>Sorry, I can't hear you</p>
-              </div>
-            </div>
-            <div className="dialog">
-              <div className="smp1">
-                <h4>Miguel</h4>
-              </div>
-              <div className="smp2">
-                <p>Without a beard i seem younger, but i'm still professional</p>
-              </div>
-            </div>
-            <div className="dialog">
-              <div className="smp1">
-                <h4>You</h4>
-              </div>
-              <div className="smp2">
-                <p>Sorry, I can't hear you</p>
-              </div>
-            </div>
-            <div className="dialog">
-              <div className="smp1">
-                <h4>Miguel</h4>
-              </div>
-              <div className="smp2">
-                <p>Without a beard i seem younger, but i'm still professional</p>
-              </div>
-            </div>
-            <div className="dialog">
-              <div className="smp1">
-                <h4>You</h4>
-              </div>
-              <div className="smp2">
-                <p>Sorry, I can't hear you</p>
-              </div>
-            </div>
-            <div className="dialog">
-              <div className="smp1">
-                <h4>Miguel</h4>
-              </div>
-              <div className="smp2">
-                <p>Without a beard i seem younger, but i'm still professional</p>
-              </div>
-            </div>
-            <div className="dialog">
-              <div className="smp1">
-                <h4>You</h4>
-              </div>
-              <div className="smp2">
-                <p>Sorry, I can't hear you</p>
-              </div>
-            </div>
-            <div className="dialog">
-              <div className="smp1">
-                <h4>Miguel</h4>
-              </div>
-              <div className="smp2">
-                <p>Without a beard i seem younger, but i'm still professional</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <style jsx>{stylesheet}</style>
-      </div>
-    );
-  }
-}
+const VideoChat = () =>
+  (
+    <div className="container">
+      <VideoBox />
+      <ProfileBox />
+      <ChatBox />
+      <style jsx>{stylesheet}</style>
+    </div>);
 
 export default VideoChat;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,14 @@ import Layout from '../components/Layout';
 import VideoChat from '../components/VideoChat';
 
 import getReduxStore from 'data/getReduxStore';
+import VideoChatPage from './VideoChatPage';
 
 class Homepage extends Page {
   render() {
     return (
       <Provider store={getReduxStore({})}>
         <Layout title="Eduible" session={this.props.session}>
-          <VideoChat />
+          <VideoChatPage />
         </Layout>
       </Provider>
     );
