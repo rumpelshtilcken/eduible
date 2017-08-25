27e74bea64688525545540062daa04cad017856a
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-25 21:19:09 +0600

    feat(Mailer): doesn't send an email on test mode

--- server/mailer.js
+++ server/mailer.js
@@ -1,27 +1,35 @@
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
+import config from 'config';
 
 // Create transport for all email activities
 // TODO: use host
 // Write your own email
-const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: 'adilkhankenzhetaev@gmail.com',
-    pass: 'R33030adcw14a14027!'
-  }
-});
-
-// send mail with defined transport object
-const emailConfirmation = (userEmail, activationCode, callback) => {
-  const mailOptions = {
-    from: { name: 'Eduible', address: 'adilkhankenzhetaev@gmail.com' }, // sender address
-    to: userEmail, // list of receivers
-    subject: 'Hello, email verification',
-    text: `Thank you for registration! It is your activation code ${activationCode},If not you please click link below`,
-    html: `Thank you for registration! It is your activation code ${activationCode},If not you please click link below`
-  };
-
-  transporter.sendMail(mailOptions, callback);
+// eslint-disable-next-line
+let emailConfirmation = (userEmail, activationCode, callback) => {
+  callback(null, { rejected: [] });
 };
+
+if (config.NODE_ENV !== 'test') {
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'adilkhankenzhetaev@gmail.com',
+      pass: 'R33030adcw14a14027!'
+    }
+  });
+
+  // send mail with defined transport object
+  emailConfirmation = (userEmail, activationCode, callback) => {
+    const mailOptions = {
+      from: { name: 'Eduible', address: 'adilkhankenzhetaev@gmail.com' }, // sender address
+      to: userEmail, // list of receivers
+      subject: 'Hello, email verification',
+      text: `Thank you for registration! It is your activation code ${activationCode},If not you please click link below`,
+      html: `Thank you for registration! It is your activation code ${activationCode},If not you please click link below`
+    };
+
+    transporter.sendMail(mailOptions, callback);
+  };
+}
 
 export default emailConfirmation;
