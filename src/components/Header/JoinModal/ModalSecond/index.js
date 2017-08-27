e293ff7739088d54408839f5e404d334675ed9f6
Turan Nurtayev <psxi@MacBook-Pro-Turan.local>
2017-08-27 15:00:33 +0600

    feat: videochat refactoring completing changes

--- src/components/Header/JoinModal/ModalSecond/index.js
+++ src/components/Header/JoinModal/ModalSecond/index.js
@@ -1,38 +1,32 @@
+import Modal from 'react-modal';
 import styles from './index.css';
 
-const ModalSuccess = () => (
-  <div className="secondSign">
-    <p className="signUpText">JOIN AS PROFESSIONAL</p>
-    <div className="firstTxt">
-      <p className="share">Share your knowledge and experience. Start now - it’s free</p>
-    </div>
-    <div className="columns">
-      <div className="firstColumn">
-        <form className="codeForm">
-          <p>JOB TITLE</p>
-          <input
-            type="string"
-            name="fullname"
-            className="codeInput"
-            placeholder="IT developer"
-          />
-          <p className="company">COMPANY</p>
-          <input
-            type="string"
-            name="fullname"
-            className="codeInput"
-            placeholder="facebook"
-          />
-        </form>
+const ModalSecond = ({ isOpen }) => (
+  <Modal
+    isOpen={isOpen}
+    onRequestClose={this.handleRequestClose}
+    className="JoinModal"
+    overlayClassName="OverlayModal"
+  >
+    <div className="secondSign">
+      <p className="signUpText">JOIN AS PROFESSIONAL</p>
+      <div className="firstTxt">
+        <p className="share">Share your knowledge and experience. Start now - it’s free</p>
       </div>
       <div className="secondColumn">
-        <p className="provide">Providing your job title and company name  will help users find you on Eduible</p>
-        <button className="continueButton" onClick="openThird()">CONTINUE</button>
-        <a href="" className="ssylka">skip this step</a>
+        <p className="provide">
+          Providing your job title and company name will help users find you on Eduible
+        </p>
+        <button className="continueButton" onClick="openThird()">
+          CONTINUE
+        </button>
+        <a href="" className="ssylka">
+          skip this step
+        </a>
       </div>
+      <style jsx>{styles}</style>
     </div>
-    <style jsx>{styles}</style>
-  </div>
+  </Modal>
 );
 
-export default ModalSuccess;
+export default ModalSecond;
--- src/components/VideoChat/index.js
+++ src/components/VideoChat/index.js
@@ -1,21 +1,29 @@
-import VideoBox from './VideoBox';
-import ChatBox from './ChatBox';
-import ProfileBox from './ProfileBox';
+import { VideoBox, ProfileBox, ChatBox } from './VChat';
+import PropTypes from 'prop-types';
 import stylesheet from './index.css';
 
-const VideoChat = ({ name, city, icon, imgUrl, timer }) =>
+const VideoChat = data =>
   (
     <div className="container">
       <VideoBox />
       <ProfileBox
-        name={name}
-        city={city}
-        icon={icon}
-        imgUrl={imgUrl}
-        timer={timer}
+        name={data.name}
+        city={data.city}
+        icon={data.icon}
+        imgUrl={data.imgUrl}
+        timer={data.timer}
       />
       <ChatBox />
       <style jsx>{stylesheet}</style>
     </div>);
 
+VideoChat.propTypes = {
+  data: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgUrl: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+    timer: PropTypes.objectOf(PropTypes.number)
+  })
+};
 export default VideoChat;
