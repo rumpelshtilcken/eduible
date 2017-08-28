fee551ceadf505a2812e445aedd02faa3a61ee62
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-28 10:24:14 +0600

    fix(VideoChat): fix props destructure

--- src/components/VideoChat/VChat/ProfileBox/index.js
+++ src/components/VideoChat/VChat/ProfileBox/index.js
@@ -1,40 +1,46 @@
 import PropTypes from 'prop-types';
+
 import stylesheet from './index.css';
 
-const ProfileBox = data => (
-  <div className="section2">
+const ProfileBox = ({ user }) =>
+  (<div className="section2">
     <div className="profile-img">
-      <img
-        src={data.imgUrl}
-        alt="profileImg"
-      />
+      <img src={user.imgUrl} alt="profileImg" />
     </div>
     <div className="profile-info">
       <div>
-        <p>{data.name.toUpperCase()}</p>
+        <p>
+          {user.name.toUpperCase()}
+        </p>
       </div>
       <div className="profile-city">
-        <img
-          src={data.icon}
-          alt="icon"
-        />
-        <p id="city">{data.city.toUpperCase()}</p>
+        <img src={user.icon} alt="icon" />
+        <p id="city">
+          {user.city.toUpperCase()}
+        </p>
       </div>
     </div>
     <div className="profile-timer">
-      <p id="timer">{data.timer.hour}:{data.timer.minutes}</p>
+      <p id="timer">
+        {user.timer.hour}:{user.timer.minutes}
+      </p>
     </div>
-    <style jsx>{stylesheet}</style>
-  </div>
-);
+    <style jsx>
+      {stylesheet}
+    </style>
+  </div>);
 
 ProfileBox.propTypes = {
-  data: PropTypes.shape({
+  user: PropTypes.shape({
     name: PropTypes.string.isRequired,
     imgUrl: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired,
     city: PropTypes.string.isRequired,
-    timer: PropTypes.objectOf(PropTypes.number)
+    timer: PropTypes.shape({
+      hour: PropTypes.number,
+      minutes: PropTypes.number
+    }).isRequired
   })
 };
+
 export default ProfileBox;
