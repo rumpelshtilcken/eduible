c0c147f2d8b7849136438c23ce05964d8a944e25
Turan Nurtayev <psxi@MacBook-Pro-Turan.local>
2017-08-28 14:01:27 +0600

    feat: turan-videochat

--- src/components/Header/JoinModal/index.js
+++ src/components/Header/JoinModal/index.js
@@ -1,47 +1,24 @@
-import { Component } from 'react';
-import Modal from 'react-modal';
 import PropTypes from 'prop-types';
 
-import ModalFirst from './ModalFirst';
-import stylessheet from './index.css';
+import Form1 from './Form1';
 
-// const validator = require('email-validator');
 
-require('isomorphic-fetch');
-
-class JoinModal extends Component {
-    state = {
-      showSuccess: false,
-      emailInput: '',
-      passwordInput: '',
-      confirmPasswordInput: ''
-    };
-
-    handleRequestClose = () => {
-      this.setState({ showSuccess: false });
-      this.props.onRequestClose();
-    }
-
-    render() {
-      return (
-        <Modal
-          isOpen={this.props.isModalJoinOpen}
-          onRequestClose={this.handleRequestClose}
-          className="JoinModal"
-          overlayClassName="OverlayModal"
-        >
-          <ModalFirst />
-          <style global>{stylessheet}</style>
-        </Modal>
-      );
-    }
-}
+const JoinModal = data => (
+  <Form1
+    isOpen={data.isOpen}
+    onRequestClose={data.onRequestClose}
+    className={data.className}
+    overlayClassName={data.overlayClassName}
+  />
+);
 
 JoinModal.propTypes = {
-  isModalJoinOpen: PropTypes.bool.isRequired,
-  className: PropTypes.string.isRequired,
-  overlayClassName: PropTypes.string.isRequired,
-  onRequestClose: PropTypes.func.isRequired
+  data: PropTypes.shape({
+    isOpen: PropTypes.bool.isRequired,
+    className: PropTypes.string.isRequired,
+    onRequestClose: PropTypes.func.isRequired,
+    overlayClassName: PropTypes.string.isRequired
+  })
 };
 
 export default JoinModal;
