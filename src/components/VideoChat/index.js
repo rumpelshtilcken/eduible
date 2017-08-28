ecfa1a9ffb7ab2fcb16350e7cc6559d014f5c6dd
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-28 10:24:14 +0600

    fix(VideoChat): fix props destructure

--- src/components/VideoChat/index.js
+++ src/components/VideoChat/index.js
@@ -1,29 +1,28 @@
-import { VideoBox, ProfileBox, ChatBox } from './VChat';
 import PropTypes from 'prop-types';
+
+import { VideoBox, ProfileBox, ChatBox } from './VChat';
 import stylesheet from './index.css';
 
-const VideoChat = data =>
-  (
-    <div className="container">
-      <VideoBox />
-      <ProfileBox
-        name={data.name}
-        city={data.city}
-        icon={data.icon}
-        imgUrl={data.imgUrl}
-        timer={data.timer}
-      />
-      <ChatBox />
-      <style jsx>{stylesheet}</style>
-    </div>);
+const VideoChat = ({ user }) =>
+  (<div className="container">
+    <VideoBox />
+    <ProfileBox user={user} />
+    <ChatBox />
+    <style jsx>
+      {stylesheet}
+    </style>
+  </div>);
 
 VideoChat.propTypes = {
-  data: PropTypes.shape({
+  user: PropTypes.shape({
     name: PropTypes.string.isRequired,
     imgUrl: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired,
     city: PropTypes.string.isRequired,
-    timer: PropTypes.objectOf(PropTypes.number)
+    timer: PropTypes.shape({
+      hour: PropTypes.number.isRequired,
+      minutes: PropTypes.number.isRequired
+    })
   })
 };
 export default VideoChat;
