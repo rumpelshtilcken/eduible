9d8c300d3113edbcc0069b9930309f03adfa2d51
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-28 10:23:33 +0600

    style: sort order of imports

--- src/components/VideoChat/VChat/ChatBox/Dialog/TextInputBox.js
+++ src/components/VideoChat/VChat/ChatBox/Dialog/TextInputBox.js
@@ -2,21 +2,17 @@ import stylesheet from './index.css';
 
 const imgUrl = '/static/sendButtonMobile.svg';
 
-
-const TextInputBox = () => (
-  <div className="dialogbox-send">
+const TextInputBox = () =>
+  (<div className="dialogbox-send">
     <div>
       <input className="input-elem" type="text" placeholder="I can't hear you!" />
     </div>
     <div>
-      <img
-        className="send-button"
-        src={imgUrl}
-        alt={'send-button'}
-      />
+      <img className="send-button" src={imgUrl} alt={'send-button'} />
     </div>
-    <style jsx>{stylesheet}</style>
-  </div>
-);
+    <style jsx>
+      {stylesheet}
+    </style>
+  </div>);
 
 export default TextInputBox;
