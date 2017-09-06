b44626a60d2e736e27331610f2d7732a231f3510
JUSTTAIREX-PC\Galym <justtairex@gmail.com>
2017-09-06 15:30:21 +0600

    feat: Validation for payment details

--- app/utils/ValidationUtils.js
+++ app/utils/ValidationUtils.js
@@ -3,12 +3,16 @@
 const PASSWORD_MIN_LENGTH = 6;
 
 const ValidationUtils = {
-  isValidEmail: (candidate: string) =>
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+  isValidEmail: candidate =>
+    /^([A-Za-z0-9]{1,})(([.]|[-]|[_]){1}[A-Za-z0-9]+){1,}[@]{1}([A-z0-9]+)([.][A-z0-9]+)+$/.test(
       candidate,
     ),
-  isValidPassword: (candidate: string) =>
-    candidate.length >= PASSWORD_MIN_LENGTH
+  isValidPassword: candidate =>
+    candidate.length >= PASSWORD_MIN_LENGTH,
+  isValidCardNumber: candidate => /^(?:\d[ -]*?){12,18}$/.test(candidate),
+  isValidCVV: candidate => /^(?:\d[ -]*?){3}$/.test(candidate),
+  isCardExpired: candidate => console.log('expired'),
+  isValidName: candidate => /^[A-Z](('[A-Z][a-z]+)|([a-z]+)|(.[ ][A-Z][a-z]+))(-[A-Z][a-z]+)?([ ][A-Z][.])?([ ][A-Z][a-z]+)((-[A-Z][a-z]+)?)$/.test(candidate)
 };
 
 export default ValidationUtils;
