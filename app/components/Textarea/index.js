b7b0c773e46450a56410b6ca783d6bdc43c0817e
adilkhan <adilkhankenzhetaev@gmail.com>
2017-09-21 22:31:21 +0600

    style(Textarea): fix eslint

--- app/components/Textarea/index.js
+++ app/components/Textarea/index.js
@@ -2,13 +2,12 @@ import PropTypes from 'prop-types';
 
 import stylessheet from './index.css';
 
-const Textarea = ({ placeholder, value, onChange }) =>
-  (<div className="textareaContainer">
+const Textarea = ({ placeholder, value, onChange }) => (
+  <div className="textareaContainer">
     <textarea className="textarea" value={value} onChange={onChange} placeholder={placeholder} />
-    <style jsx>
-      {stylessheet}
-    </style>
-  </div>);
+    <style jsx>{stylessheet}</style>
+  </div>
+);
 
 Textarea.propTypes = {
   placeholder: PropTypes.string,
