c413ec99197b85f8224673853800e84c41f1b67a
adilkhan <adilkhankenzhetaev@gmail.com>
2017-09-21 22:29:57 +0600

    feat(Layout): move links to the Layout

--- app/components/Layout/index.js
+++ app/components/Layout/index.js
@@ -1,75 +1,63 @@
+import { Component } from 'react';
 import Head from 'next/head';
-import { PropTypes } from 'react';
+import PropTypes from 'prop-types';
 
-import Header from '../Header';
-import Footer from '../Footer';
+import { Header, Footer } from 'components';
 import stylesheet from './index.css';
 
-const headerLinkss = [
-  { label: 'menu1', link: '#' },
-  { label: 'menu2', link: '#' },
-  { label: 'menu3', link: '#' },
-  { label: 'menu4', link: '#' }
-];
-const footerLinkss = [
-  { title: 'ABOUT', label1: 'How It Works', link1: '#', label2: 'Succsess Stories', link2: '#' },
-  {
-    title: 'PROFESSIONALS',
-    label1: 'Join As Professional',
-    link1: '#',
-    label2: 'Search For Professional',
-    link2: '#'
-  },
-  {
-    title: 'UNIVERSITIES',
-    label1: 'Create Account',
-    link1: '#',
-    label2: 'Search University',
-    link2: '#'
-  },
-  { title: 'ANSWERS', label1: "FAQ's", link1: '#', label2: 'Privacy Policy', link2: '#' }
-];
+class Layout extends Component {
+  headerLinks = [
+    { label: 'menu1', link: '#' },
+    { label: 'menu2', link: '#' },
+    { label: 'menu3', link: '#' },
+    { label: 'menu4', link: '#' }
+  ];
+  footerLinks = [
+    { title: 'ABOUT', label1: 'How It Works', link1: '#', label2: 'Succsess Stories', link2: '#' },
+    {
+      title: 'PROFESSIONALS',
+      label1: 'Join As Professional',
+      link1: '#',
+      label2: 'Search For Professional',
+      link2: '#'
+    },
+    {
+      title: 'UNIVERSITIES',
+      label1: 'Create Account',
+      link1: '#',
+      label2: 'Search University',
+      link2: '#'
+    },
+    { title: 'ANSWERS', label1: "FAQ's", link1: '#', label2: 'Privacy Policy', link2: '#' }
+  ];
 
-const Layout = (
-  { children, title = 'Eduible', session = false, headerLinks, footerLinks } = {}
-) => (
-  <div className="content">
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link href="https://fonts.googleapis.com/css?family=Effra" rel="stylesheet" />
-    </Head>
-    <Header headerLinks={headerLinks || headerLinkss} />
+  render() {
+    const { children, title = 'Eduible' } = this.props;
 
-    {children}
-    <Footer footerLinks={footerLinks || footerLinkss} copyright={'COPYRIGHT (C) 2017'} />
+    return (
+      <div className="content">
+        <Head>
+          <title>{title}</title>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link href="static/Fonts/Effra" rel="stylesheet" />
+        </Head>
 
-    <style jsx global>
-      {stylesheet}
-    </style>
-  </div>
-);
+        <Header headerLinks={this.headerLinks} />
+        {children}
+        <Footer footerLinks={this.footerLinks} copyright={'COPYRIGHT (C) 2017'} />
+
+        <style jsx global>
+          {stylesheet}
+        </style>
+      </div>
+    );
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
-  title: PropTypes.string,
-  session: PropTypes.object.isRequired,
-  footerLinks: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      link1: PropTypes.string.isRequired,
-      label1: PropTypes.string.isRequired,
-      link2: PropTypes.string.isRequired,
-      label2: PropTypes.string.isRequired
-    })
-  ).isRequired,
-  headerLinks: PropTypes.arrayOf(
-    PropTypes.shape({
-      label: PropTypes.string,
-      link: PropTypes.string
-    })
-  )
+  title: PropTypes.string
 };
 
 export default Layout;
