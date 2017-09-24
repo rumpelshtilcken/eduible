f452c70406973d28a477db6e74c299dc815a1015
adilkhan <adilkhankenzhetaev@gmail.com>
2017-09-25 05:56:30 +0600

    feat: introduce auth0Config

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,11 @@
 // Graphcool
 export const apiUrl = 'https://api.graph.cool/simple/v1/cj7x06p6e0rin0138204qqj4x';
 
-// Auth0
-export const domain = 'eduible.auth0.com';
-export const clientId = 'Z1VjYlw0k76XQY1Ni1y8Q4llfEzp0WvS';
+export const auth0Config = {
+  domain: 'eduible.auth0.com',
+  clientId: 'Z1VjYlw0k76XQY1Ni1y8Q4llfEzp0WvS',
+  callbackUrl: 'http://localhost:3000',
+  realm: 'Username-Password-Authentication',
+  apiUrl: 'http://localhost:3001',
+  scope: 'openid email create:token'
+};
