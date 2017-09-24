c424423f3db64bb550730f8426de547a31bd1737
adilkhan <adilkhankenzhetaev@gmail.com>
2017-09-25 04:07:00 +0600

    feat(pages): connect withPage hoc

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,15 +1,11 @@
-import { Provider } from 'react-redux';
 import Head from 'next/head';
 
-import getReduxStore from 'data/getReduxStore';
 import withPage from 'hoc/withPage';
 
 const Homepage = () => (
-  <Provider store={getReduxStore({})}>
-    <Head>
-      <title>{'Eduible'}</title>
-    </Head>
-  </Provider>
+  <Head>
+    <title>{'Eduible'}</title>
+  </Head>
 );
 
 export default withPage(Homepage);
