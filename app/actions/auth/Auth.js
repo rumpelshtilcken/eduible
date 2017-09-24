207a9e017b0ff6f68c57f90ee47807dedea357d7
adilkhan <adilkhankenzhetaev@gmail.com>
2017-09-25 05:55:11 +0600

    feat: introduce Auth action

---
+++ app/actions/auth/Auth.js
@@ -0,0 +1,74 @@
+import auth0 from 'auth0-js';
+import { auth0Config } from 'config';
+
+export default class Auth {
+  auth0 = new auth0.WebAuth({
+    domain: auth0Config.domain,
+    clientID: auth0Config.clientId,
+    // audience: `https://${auth0Config.domain}/userinfo`,
+    redirectUri: auth0Config.callbackUrl,
+    scope: auth0Config.scope,
+    responseType: 'token id_token'
+  });
+
+  constructor() {
+    this.signin = this.signin.bind(this);
+    this.signout = this.signout.bind(this);
+    this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
+  }
+
+  signin(username, password) {
+    return new Promise((resolve, reject) => {
+      this.auth0.client.login(
+        { realm: auth0Config.realm, username, password },
+        (err, authResult) => {
+          if (err) {
+            console.log(err);
+            return reject(err);
+          }
+          console.log(authResult);
+          this.setSession(authResult);
+          return resolve();
+        }
+      );
+    });
+  }
+
+  signout() {
+    // Clear access token and ID token from local storage
+    localStorage.removeItem('origin_access_token');
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+  }
+
+  handleAuthentication() {
+    return new Promise((resolve, reject) => {
+      this.auth0.parseHash((err, authResult) => {
+        if (authResult && authResult.accessToken && authResult.idToken) {
+          this.setSession(authResult);
+          return resolve();
+        } else if (err) {
+          console.log(err);
+          return reject(err);
+        }
+      });
+    });
+  }
+
+  setSession(authResult) {
+    // Set the time that the access token will expire at
+    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+  }
+
+  isAuthenticated() {
+    // Check whether the current time is past the
+    // access token's expiry time
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+}
---
+++ app/actions/auth/index.js
@@ -0,0 +1,73 @@
+import fetch from 'isomorphic-unfetch';
+
+import { auth0Config } from 'config';
+import { getAccessToken } from 'utils/auth';
+
+import {
+  AUTH_USER,
+  AUTH_IN_PROGRESS,
+  UNAUTH_USER,
+  AUTH_ERROR,
+  CLEARDOWN
+} from './authTypes';
+
+import Auth from './Auth';
+
+const auth = new Auth();
+
+export const signinUser = ({ email, password }, callback) => (dispatch) => {
+  dispatch({ type: AUTH_IN_PROGRESS });
+
+  auth.signin(email, password, callback)
+    .then(() => {
+      const auth0AccessToken = getAccessToken();
+
+      fetch(`${auth0Config.apiUrl}/getOriginAccessToken`, {
+        headers: {
+          Authorization: `Bearer ${auth0AccessToken}`
+        }
+      })
+        .then((res) => {
+          const { success, originAccessToken } = res.data;
+
+          if (success === 'true') {
+            localStorage.setItem('origin_access_token', originAccessToken);
+            dispatch({ type: AUTH_USER });
+            return callback();
+          }
+          return dispatch(authError('Error fetching Origin Access Token'));
+        })
+        .catch((error) => {
+          const errorMsg = error.description || error.message || 'Unspecified error';
+          return dispatch(authError(errorMsg));
+        });
+    })
+    .catch((error) => {
+      const errorMsg = error.description || error.message || 'Unspecified error';
+      return dispatch(authError(errorMsg));
+    });
+};
+
+export const authError = (error) => {
+  const timestamp = Date.now();
+  return {
+    type: AUTH_ERROR,
+    error,
+    timestamp
+  };
+};
+
+export const signoutUser = () => {
+  auth.signout();
+  return { type: UNAUTH_USER };
+};
+
+export const cleardown = () => ({
+  type: CLEARDOWN
+});
+
+export const handleAuthentication = callback => (dispatch) => {
+  auth.handleAuthentication()
+    .then(() => dispatch({ type: AUTH_USER }))
+    .catch(err => dispatch({ type: UNAUTH_USER }));
+};
