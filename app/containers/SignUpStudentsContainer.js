0f1d48c891287fc61a003698838bfaa0023789e3
adilkhan <adilkhankenzhetaev@gmail.com>
2017-09-25 04:06:21 +0600

    feat: introduce SignUpStudentsContainer

---
+++ app/containers/SignUpStudentsContainer.js
@@ -0,0 +1,56 @@
+import { Component } from 'react';
+
+import { SignUpStudent } from 'components';
+import getWebAuth from 'lib/getWebAuth';
+
+class SignUpStudentContainer extends Component {
+  handleContinueButtonClick = async ({
+    fullname,
+    date,
+    email,
+    password
+  }) => {
+    try {
+      const webAuth = await getWebAuth();
+      const result = await webAuth.signup({
+        connection: 'Username-Password-Authentication',
+        email,
+        password
+      }, (err) => {
+        if (err) return console.log('Error:|| ', err);
+        // TODO: login user
+        // TODO: save data to graphcool
+        return console.log('success signup without login!');
+      });
+
+      console.log('Result: ', result);
+      console.log('Result: ', result.response);
+      console.log('Result: ', result.json());
+    } catch (error) {
+      console.log('Error catch: ', error);
+    }
+  };
+
+  handleLinkedinButtonClick = () => {
+    getWebAuth().authorize({
+      connection: 'linkedin'
+    });
+  };
+
+  handleLoginButtonClick = () => {
+    // TODO: open Login modal
+  };
+
+  render() {
+    return (
+      <SignUpStudent
+        onContinueButtonClick={this.handleContinueButtonClick}
+        onLinkedinButtonClick={this.handleLinkedinButtonClick}
+        onLoginButtonClick={this.handleLoginButtonClick}
+        {...this.props}
+      />
+    );
+  }
+}
+
+export default SignUpStudentContainer;
