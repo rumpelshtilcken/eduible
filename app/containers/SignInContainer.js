f96bf44017e9a657ac0b0542e2c31c882b577033
adilkhan <adilkhankenzhetaev@gmail.com>
2017-09-25 04:05:18 +0600

    feat(SignInContainer): connect webAuth

--- app/containers/SignInContainer.js
+++ app/containers/SignInContainer.js
@@ -5,21 +5,28 @@ import { SignIn } from 'components';
 import getWebAuth from 'lib/getWebAuth';
 
 class SignInContainer extends Component {
+  componentWillMount() {
+    this.initAuth();
+  }
+
+  initAuth = async () => {
+    this.webAuth = await getWebAuth();
+  };
+
   handleContinueButtonClick = async ({ email, password }) => {
     try {
-      const webAuth = await getWebAuth();
-      console.log(email, password);
-      webAuth.client.login({
-
+      this.webAuth.client.login({
+        realm: 'Username-Password-Authentication',
         username: email,
         password,
         scope: 'openid profile',
         audience: 'urn:test'
       }, (err, authResult) => {
-        // Auth tokens in the result or an error
         if (err) return console.log('Error: ', err);
         // TODO: save to graphcool
+        // authResult.accessToken;
         console.log('AuthResult: ', authResult);
+        console.log('AuthResult: ', authResult.accessToken);
       });
     } catch (err) {
       console.log('Catch handler: ', err);
@@ -28,10 +35,12 @@ class SignInContainer extends Component {
 
   handleFacebookButtonClick = () => {
     // TODO: facebook login
+    this.webAuth.authorize({ connection: 'facebook' });
   };
 
   handleGoogleButtonClick = () => {
     // TODO: google login
+    this.webAuth.authorize({ connection: 'google' });
   };
 
   render() {
