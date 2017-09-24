4f70e638d926d4a7e633f46f2e02045f290de8ae
adilkhan <adilkhankenzhetaev@gmail.com>
2017-09-24 09:46:28 +0600

    feat(ResponsiveMenu): remove outline of buttons

---
+++ app/components/ResponsiveMenu/index.js
@@ -0,0 +1,69 @@
+import { Component } from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+
+import { Link } from 'components';
+
+import MobileBurgerMenu from './MobileBurgerMenu';
+import stylesheet from './index.css';
+
+class ResponsiveMenu extends Component {
+  state = {
+    isMenuHidden: true
+  };
+
+  handleBurgerMenuClick = () => this.setState({ isMenuHidden: !this.state.isMenuHidden });
+
+  render() {
+    const { links, buttons } = this.props;
+    return (
+      <div>
+        <div className="mobileMenuContainer">
+          <MobileBurgerMenu
+            onClick={this.handleBurgerMenuClick}
+            isCloseState={!this.state.isMenuHidden}
+          />
+        </div>
+        <div className={cx('linksContainer', {
+          hideLinks: this.state.isMenuHidden
+        })}
+        >
+          {links && links.map(({ url, title, prefetch }) => (
+            <div key={title} className="linkContainer">
+              <Link href={url} prefetch={prefetch} white>
+                {title}
+              </Link>
+            </div>
+          ))}
+
+          {buttons && buttons.map(({ title, onClick }) => (
+            <div key={title} className="linkContainer">
+              <button
+                className="button"
+                onClick={onClick}
+              >
+                {title.toUpperCase()}
+              </button>
+            </div>
+          ))}
+        </div>
+
+        <style jsx>{stylesheet}</style>
+      </div>
+    );
+  }
+}
+
+ResponsiveMenu.propTypes = {
+  buttons: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired
+  })),
+  links: PropTypes.arrayOf(PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    prefetch: PropTypes.bool
+  }))
+};
+
+export default ResponsiveMenu;
