df7edb0367623a9b72d2f9ee19733580d707ec12
Turan <tnurtayev@gmail.com>
2017-09-26 01:18:21 +0600

    feat: videochat chatview-ui

--- app/components/VideoChat/VChat/ChatBox/Dialog/index.js
+++ app/components/VideoChat/VChat/ChatBox/Dialog/index.js
@@ -1,8 +1,50 @@
-import DialogBox from './DialogBox';
-import TextInputBox from './TextInputBox';
+import React from 'react';
+import { Message } from 'react-chat-ui';
 
-export {
-  DialogBox,
-  TextInputBox
-};
+import stylesheet from './index.css';
 
+const imgUrl = '/static/sendButtonMobile.svg';
+
+class TextInputBox extends React.Component {
+  state = {
+    msg: ''
+  }
+
+  handleChange =e => this.setState({ msg: e.target.value });
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ msg: '' });
+    const newMessage = new Message({ id: 0, message: this.inputRef.value });
+    this.props.onSubmitButtonClick(newMessage);
+  }
+  render() {
+    return (
+      <div className="dialogbox-send">
+        <div>
+          <input
+            className="input-elem"
+            type="text"
+            value={this.state.msg}
+            onChange={this.handleChange}
+            ref={node => this.inputRef = node}
+            placeholder="Type message ..."
+          />
+        </div>
+        <div>
+          <img
+            className="send-button"
+            src={imgUrl}
+            alt={'send-button'}
+            onClick={this.handleSubmit}
+          />
+        </div>
+        <style jsx>
+          {stylesheet}
+        </style>
+      </div>
+    );
+  }
+}
+
+export default TextInputBox;
--- app/components/VideoChat/VChat/ChatBox/index.js
+++ app/components/VideoChat/VChat/ChatBox/index.js
@@ -1,13 +1,62 @@
+import React from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
+import { ChatFeed, Message } from 'react-chat-ui';
+
+import TextInputBox from './Dialog';
 import stylesheet from './Dialog/index.css';
-import { DialogBox, TextInputBox } from './Dialog';
 
-/* eslint-disable */
-const ChatBox = ({ sendMessageTest }) => (
-  <div className="section3">
-    <TextInputBox sendMessageTest={sendMessageTest} />
-    <DialogBox />
-    <style jsx>{stylesheet}</style>
-  </div>
-);
+class ChatBox extends React.Component {
+  state = {
+    messages: [
+      (new Message({ id: 1, message: "I'm the recipient! (The person you're talking to)" })), // Gray bubble
+      (new Message({ id: 1, message: "I'm the recipient! (The person you're talking to)" })), // Gray bubble
+      (new Message({ id: 1, message: "I'm the recipient! (The person you're talking to)" })), // Gray bubble
+      (new Message({ id: 1, message: "I'm the recipient! (The person you're talking to)" })), // Gray bubble
+      (new Message({ id: 1, message: "I'm the recipient! (The person you're talking to)" })), // Gray bubble
+      (new Message({ id: 1, message: "I'm the recipient! (The person you're talking to)" })), // Gray bubble
+      (new Message({ id: 0, message: "I'm you -- the blue bubble!" })),
+      // Blue bubble,
 
+      (new Message({ id: 1, message: "I'm the recipient! (The person you're talking to)" })) // Gray bubble
+
+    ]
+  }
+  handleSubmitButtonClick = (newMessage) => {
+    const messageHistory = this.state.messages.slice();
+    messageHistory.push(newMessage);
+    this.setState({
+      messages: messageHistory
+    });
+  }
+  render() {
+    return (
+      <div className="section3" >
+        <TextInputBox onSubmitButtonClick={this.handleSubmitButtonClick} />
+        <Scrollbars style={{ width: 240, height: 289 }} autohide>
+          <div className="chatbox">
+            <ChatFeed
+              messages={this.state.messages} // Boolean: list of message objects
+              isTyping={this.state.is_typing} // Boolean: is the recipient typing
+              hasInputField={false} // Boolean: use our input, or use your own
+              bubblesCentered={false} // Boolean should the bubbles be centered in the feed?
+              // JSON: Custom bubble styles
+              bubbleStyles={
+                {
+                  text: {
+                    fontSize: 10
+                  },
+                  chatbubble: {
+                    borderRadius: 20,
+                    padding: 7
+                  }
+                }
+              }
+            />
+          </div>
+        </Scrollbars>
+        <style jsx>{stylesheet}</style>
+      </div>
+    );
+  }
+}
 export default ChatBox;
