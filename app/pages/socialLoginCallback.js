98f928a5f15e59e0df9118144ae50a08ecbfb73e
adilkhan <adilkhankenzhetaev@gmail.com>
2017-09-27 06:49:49 +0600

    feat: introduce socialLoginCallback

---
+++ app/pages/socialLoginCallback.js
@@ -0,0 +1,55 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import * as actions from 'actions/auth';
+import { connect } from 'react-redux';
+import withData from 'lib/withData';
+
+class SocialLoginCallback extends Component {
+  componentDidMount() {
+    const hash = window.location.hash;
+    this.props.socialSignInCallback(hash);
+  }
+
+  render() {
+    console.log(
+      this.props
+    );
+    if (this.props.loading) {
+      return (
+        <div>
+          {'Loading'}
+        </div>);
+    }
+
+    if (this.props.error) {
+      return (
+        <div>
+          {'Error'}
+        </div>);
+    }
+
+    return (
+      <div>
+        {'Thank you for sign up, please close the window'}
+      </div>
+    );
+  }
+}
+
+SocialLoginCallback.propTypes = {
+  socialSignInCallback: PropTypes.func.isRequired,
+  loading: PropTypes.bool
+};
+
+const mapStateToProps = (state) => {
+  const { error, timestamp, forgotMsg, loading } = state.auth;
+  return {
+    error,
+    timestamp,
+    forgotMsg,
+    loading
+  };
+};
+
+export default withData(connect(mapStateToProps, actions)(SocialLoginCallback));
