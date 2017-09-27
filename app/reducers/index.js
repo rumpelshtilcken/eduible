856b1fd5318b2795d5be17b393ebbaf0dba176b5
adilkhan <adilkhankenzhetaev@gmail.com>
2017-09-27 06:52:21 +0600

    fix(reducers): export authReducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,18 +1,4 @@
 import modalReducer from './modalReducer';
+import auth from './authReducer';
 
-const EMAIL_CHANGED = 'EMAIL_CHANGED';
-
-const INITIAL_STATE = {
-  email: ''
-};
-
-const example = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case EMAIL_CHANGED:
-      return { ...state, email: action.payload };
-    default:
-      return state;
-  }
-};
-
-export default { auth: example, modal: modalReducer };
+export default { auth, modal: modalReducer };
