29c0ceb4664949f5cfd66349922ae18a6cf2ce6a
adilkhan <adilkhankenzhetaev@gmail.com>
2017-09-27 06:48:49 +0600

    feat(config): update config scope

--- app/config/index.js
+++ app/config/index.js
@@ -3,9 +3,9 @@ export const apiUrl = 'https://api.graph.cool/simple/v1/cj7x06p6e0rin0138204qqj4
 
 export const auth0Config = {
   domain: 'eduible.auth0.com',
-  clientId: 'Z1VjYlw0k76XQY1Ni1y8Q4llfEzp0WvS',
-  callbackUrl: 'http://localhost:3000',
+  clientID: 'Z1VjYlw0k76XQY1Ni1y8Q4llfEzp0WvS',
+  callbackUrl: 'http://localhost:3000/socialLoginCallback',
   realm: 'Username-Password-Authentication',
-  apiUrl: 'http://localhost:3001',
-  scope: 'openid email create:token'
+  scope: 'openid email create:token profile app_metadata user_metadata',
+  redirectUri: 'http://localhost:3000/socialLoginCallback'
 };
