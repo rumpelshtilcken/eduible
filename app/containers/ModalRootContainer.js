3185ccc0943bd5c1f814fd243e7e6388c9fb5cb6
adilkhan <adilkhankenzhetaev@gmail.com>
2017-09-27 06:49:27 +0600

    feat(ModalRootContainer): add SignUpStudentContainer

--- app/containers/ModalRootContainer.js
+++ app/containers/ModalRootContainer.js
@@ -2,13 +2,15 @@ import { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import SignUpProfessionalContainer from './SignUpProfessionalContainer';
 import SignInContainer from './SignInContainer';
+import SignUpContainerStudentContainer from './SignUpStudentsContainer';
+import SignUpProfessionalContainer from './SignUpProfessionalContainer';
 
 class ModalRootContainer extends Component {
   MODAL_COMPONENTS = {
     SIGN_UP_PROFESSIONALS: SignUpProfessionalContainer,
-    SIGN_IN: SignInContainer
+    SIGN_IN: SignInContainer,
+    SIGN_UP_STUDENTS: SignUpContainerStudentContainer
   };
 
   render() {
