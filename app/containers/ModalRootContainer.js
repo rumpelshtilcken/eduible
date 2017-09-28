9ed5f9633a1fe7e7d2c8009354b6933ecf1131d9
adilkhan <adilkhankenzhetaev@gmail.com>
2017-09-28 10:07:04 +0600

    feat(ModalRootContainer): connect to modalActions

--- app/containers/ModalRootContainer.js
+++ app/containers/ModalRootContainer.js
@@ -1,34 +1,46 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 
+import * as actions from 'actions/modal';
+
 import SignInContainer from './SignInContainer';
-import SignUpContainerStudentContainer from './SignUpStudentsContainer';
+import SignUpStudentsContainer from './SignUpStudentsContainer';
 import SignUpProfessionalContainer from './SignUpProfessionalContainer';
+import SignUpProfessionalStep2Container from './SignUpProfessionalStep2Container';
 
 class ModalRootContainer extends Component {
   MODAL_COMPONENTS = {
-    SIGN_UP_PROFESSIONALS: SignUpProfessionalContainer,
+    SIGN_UP_PROFESSIONAL: SignUpProfessionalContainer,
     SIGN_IN: SignInContainer,
-    SIGN_UP_STUDENTS: SignUpContainerStudentContainer
+    SIGN_UP_STUDENT: SignUpStudentsContainer,
+    SIGN_UP_PROFESSIONAL_STEP_2: SignUpProfessionalStep2Container,
+    LOADING: <div>Loading...</div>
   };
 
   render() {
-    const { modalType, modalProps } = this.props;
-    if (!modalType) {
+    const { type } = this.props;
+    if (type === null) {
       return null;
     }
 
-    const SpecificModal = this.MODAL_COMPONENTS[modalType];
-    return <SpecificModal {...modalProps} />;
+    const SpecificModal = this.MODAL_COMPONENTS[type];
+    return (
+      <SpecificModal isOpen onRequestClose={this.props.hideModal} />
+    );
   }
 }
 
 ModalRootContainer.propTypes = {
-  modalType: PropTypes.string,
-  modalProps: PropTypes.object
+  type: PropTypes.string,
+  hideModal: PropTypes.func.isRequired
 };
 
+const mapStateToProps = ({ modal }) => modal;
+const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
+
 export default connect(
-  state => state.modal
+  mapStateToProps,
+  mapDispatchToProps
 )(ModalRootContainer);
