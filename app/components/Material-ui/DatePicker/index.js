790b3c76fcd414a5cf9c4f87c4d9b08c5ca48106
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-04 04:19:30 +0600

    feat: introduce MuiDatePicker

---
+++ app/components/Material-ui/DatePicker/index.js
@@ -0,0 +1,24 @@
+import DatePicker from 'material-ui/DatePicker';
+import PropTypes from 'prop-types';
+
+const MuiDatePicker = ({ onChange, title, value }) => (
+  <DatePicker
+    floatingLabelText={title}
+    floatingLabelFixed
+    floatingLabelStyle={{ fontSize: '12px', color: '#626262' }}
+    hintText={value}
+    hintStyle={{ fontSize: '11px' }}
+    inputStyle={{ fontSize: '11px' }}
+    mode="landscape"
+    fullWidth
+    onChange={onChange}
+  />
+);
+
+MuiDatePicker.propTypes = {
+  onChange: PropTypes.func,
+  title: PropTypes.string,
+  value: PropTypes.string
+};
+
+export default MuiDatePicker;
