24a5778471d1c8c4f9ec000afda1035448269c3a
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-04 02:46:23 +0600

    feat(SignUpProfessionalStep2Container): refactor queries and mutations

--- app/containers/SignUpProfessionalStep2Container.js
+++ app/containers/SignUpProfessionalStep2Container.js
@@ -1,35 +1,70 @@
-import { bindActionCreators } from 'redux';
 import { Component } from 'react';
 import { connect } from 'react-redux';
-import { graphql, gql } from 'react-apollo';
+import { compose, graphql, gql } from 'react-apollo';
 import PropTypes from 'prop-types';
 
 import { SignUpProfessionalStep2 } from 'components';
-import * as authActions from 'actions/auth';
 import * as modalActions from 'actions/modal';
 import { getCurrentUserData } from 'utils/auth';
 
 class SignUpProfessionalStep2Container extends Component {
-  handleContinueButtonClick = ({
+  static propTypes = {
+    data: PropTypes.shape({
+      loading: PropTypes.bool.isRequired,
+      allUsers: PropTypes.arrayOf(PropTypes.shape({
+        professional: PropTypes.shape({
+          id: PropTypes.string
+        })
+      }))
+    }).isRequired,
+    getProfessionalsJob: PropTypes.func.isRequired,
+    updateProfessionalJob: PropTypes.func.isRequired,
+    showSignUpProfessionalUniversity: PropTypes.func.isRequired
+  };
+
+  handleContinueButtonClick = async ({
     jobTitle,
     company
   }) => {
-    const variables = {
-      id: this.props.data.allUsers[0].professional.id,
-      job: {
-        jobTitle: { title: jobTitle },
-        company: { name: company }
-      }
-    };
+    try {
+      const variables = await this.prepareVariables({
+        id: this.props.data.allUsers[0].professional.id,
+        jobTitle,
+        company
+      });
 
-    this.props.updateJobTitle({ variables }).then(
-      this.props.showSignUpProfessionalUniversity
-    );
+      await this.props.updateProfessionalJob({ variables });
+      this.props.showSignUpProfessionalUniversity();
+    } catch (err) {
+      // TODO: show an error
+      console.log('Job update error: ', err);
+    }
   };
 
-  handleSkipButtonClick = () => {
-    this.props.showSignUpProfessionalUniversity();
+  prepareVariables = async ({ id, jobTitle, company }) => {
+    try {
+      const professionalJob = await this.props.getProfessionalsJob({
+        variables: { jobTitle, company }
+      });
+
+      return {
+        id,
+        job: professionalJob
+          ? {
+            jobTitleId: professionalJob.jobTitle.id,
+            companyId: professionalJob.company.id
+          }
+          : {
+            jobTitle: { title: jobTitle },
+            company: { name: company }
+          }
+      };
+    } catch (err) {
+      throw err;
+    }
   };
+
+  handleSkipButtonClick = () => this.props.showSignUpProfessionalUniversity();
 
   render() {
     return (
@@ -42,63 +77,61 @@ class SignUpProfessionalStep2Container extends Component {
   }
 }
 
-SignUpProfessionalStep2Container.propTypes = {
-  signupProfessional: PropTypes.func,
-  signinLinkedin: PropTypes.func,
-  showSignUpProfessionalStep2Modal: PropTypes.func,
-  data: PropTypes.object,
-  updateJobTitle: PropTypes.func,
-  showSignUpProfessionalUniversity: PropTypes.func
-};
-
-const mapStateToProps = (state) => {
-  const { error, timestamp, forgotMsg, loading } = state.auth;
-  return {
-    error,
-    timestamp,
-    forgotMsg,
-    loading
-  };
-};
-
-const mapDispatchToProps = dispatch => ({
-  ...bindActionCreators(authActions, dispatch),
-  ...bindActionCreators(modalActions, dispatch)
-});
-
 const getProfessionalByAuth0Id = gql`
-query allUsers($auth0UserId: String!) {
-  allUsers (filter: {auth0UserId: $auth0UserId}) {
-    id
-    professional {
+  query allUsers($auth0UserId: String!) {
+    allUsers (filter: {auth0UserId: $auth0UserId}) {
       id
+      professional {
+        id
+        job {
+          jobTitle { title }
+          company { name }
+        }
+      }
     }
   }
-} 
 `;
-const SignUpProfessionalStep2ContainerWithQuery = graphql(getProfessionalByAuth0Id, {
-  options: () => ({
-    variables: {
-      auth0UserId: getCurrentUserData('sub')
-    }
-  })
-})(SignUpProfessionalStep2Container);
 
-const updateJobTitle = gql`
+const getProfessionalsJob = gql`
+  query getProfessionalsJob($jobTitle: String!, $companyName: String!) {
+    jobTitle: allJobTitles(filter: {
+      title: $jobTitle
+    }) {
+      id
+      title
+    }
+    company: allCompanies(filter: {
+      name: $company
+    }) {
+      id
+      name
+    }
+  }
+`;
+
+const updateProfessionalJob = gql`
   mutation updateProfessional ($id: ID!, $job: ProfessionaljobJob) {
     updateProfessional( id: $id job: $job) {
       id
     }
-  } 
+  }
 `;
-const SignUpProfessionalStep2ContainerWithQueryAndMutaion = graphql(updateJobTitle, {
-  props: ({ ownProps, mutate }) => console.log(ownProps) || ({
-    updateJobTitle: ({ variables }) => mutate({
-      variables
+
+const EnhancedSignUpProfessionalStep2Container = compose(
+  graphql(getProfessionalByAuth0Id, {
+    skip: () => !getCurrentUserData('sub'),
+    options: () => ({ variables: { auth0UserId: getCurrentUserData('sub') } })
+  }),
+  graphql(getProfessionalsJob, {
+    options: ({ jobTitle, companyName }) => ({ variables: { jobTitle, companyName } })
+  }),
+  graphql(updateProfessionalJob, { props: ({ mutate }) =>
+    ({
+      updateProfessionalJob: ({ variables }) => mutate({ variables })
     })
   })
-})(SignUpProfessionalStep2ContainerWithQuery);
+)(SignUpProfessionalStep2Container);
 
 export default
-connect(mapStateToProps, mapDispatchToProps)(SignUpProfessionalStep2ContainerWithQueryAndMutaion);
+connect(null, modalActions)(EnhancedSignUpProfessionalStep2Container);
 
