f9840b68bbae884a1d747a9a549d8fe8302e8f81
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-07 04:34:57 +0600

    feat(TextInput): connect TextField to the redux

--- app/components/Material-ui/TextField/index.js
+++ app/components/Material-ui/TextField/index.js
@@ -1,44 +1,25 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import TextField from 'material-ui/TextField';
 
-class MuiTextField extends Component {
-  state = {
-    errorText: ''
-  };
-
-  handleChange = (event) => {
-    if (this.props.validation) {
-      const errorText = this.props.validation(event.target.value);
-      this.setState({ errorText });
-    }
-
-    return this.props.onChange && this.props.onChange(event);
-  };
-
-  render() {
-    const { type, name, title, value } = this.props;
-    return (
-      <TextField
-        errorText={this.state.errorText}
-        floatingLabelFixed
-        floatingLabelStyle={{ fontSize: '13px', color: '#626262', fontWeight: 'bold' }}
-        floatingLabelText={title}
-        fullWidth
-        hintStyle={{ fontSize: '11px' }}
-        hintText={value}
-        inputStyle={{ fontSize: '11px' }}
-        mode="landscape"
-        name={name}
-        onChange={this.handleChange}
-        type={type}
-      />
-    );
-  }
-}
+const MuiTextField = ({ type, name, title, value, errorText, onChange }) => (
+  <TextField
+    errorText={errorText}
+    floatingLabelFixed
+    floatingLabelStyle={{ fontSize: '13px', color: '#626262', fontWeight: 'bold' }}
+    floatingLabelText={title}
+    fullWidth
+    hintStyle={{ fontSize: '11px' }}
+    hintText={value}
+    inputStyle={{ fontSize: '11px' }}
+    mode="landscape"
+    name={name}
+    onChange={onChange}
+    type={type}
+  />
+);
 
 MuiTextField.propTypes = {
-  validation: PropTypes.func,
+  errorText: PropTypes.string,
   name: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired,
