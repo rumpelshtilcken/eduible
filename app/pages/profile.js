5a1d4a37d57d40b01fb5365a21161bc96f2d3602
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-09 01:47:13 +0600

    feat(profile): fetch user data according userID

--- app/pages/profile.js
+++ app/pages/profile.js
@@ -1,12 +1,21 @@
 import { Component } from 'react';
 import Head from 'next/head';
+import PropTypes from 'prop-types';
 import Router from 'next/router';
 
 import withPage from 'hoc/withPage';
 import ProfileContainer from 'containers/ProfileContainer';
 
 class ProfilePage extends Component {
-  handleProfileEditButtonClick = ({ userType, userId }) => {
+  static propTypes = {
+    url: PropTypes.shape({
+      query: PropTypes.shape({
+        userId: PropTypes.string.isRequired
+      })
+    })
+  };
+
+  handleProfileEditButtonClick = ({ userType, userId }) =>
     Router.push({
       pathname: '/profileEdit',
       query: {
@@ -14,16 +23,27 @@ class ProfilePage extends Component {
         userId
       }
     });
-  };
+
+  handleRequestCallClick = ({ professionalId }) =>
+    Router.push({
+      pathname: '/callRequest',
+      query: {
+        professionalId
+      }
+    });
 
   render() {
+    const { userId } = this.props.url.query;
+
     return (
       <div>
         <Head>
           <title>{'Profile'}</title>
         </Head>
         <ProfileContainer
-          onProfileEditButtonClick={this.handleProfileEditButtonClick}
+          userId={userId}
+          onRequestCallClick={this.handleRequestCallClick}
+          onEditButtonClick={this.handleProfileEditButtonClick}
         />
       </div>
     );
