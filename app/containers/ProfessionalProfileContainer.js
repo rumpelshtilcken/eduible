936ee4c5e97d2f42b33258b2598c9b6485b51d4b
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-09 01:45:21 +0600

    feat(ProfessionalProfileContainer): connect to graphcool

--- app/containers/ProfessionalProfileContainer.js
+++ app/containers/ProfessionalProfileContainer.js
@@ -1,67 +1,102 @@
 import { Component } from 'react';
+
 import { graphql, gql } from 'react-apollo';
 import PropTypes from 'prop-types';
 
 import { ProfessionalProfile } from 'components';
 
 class ProfessionalProfileContainer extends Component {
-  static propTypes = {
-    user: PropTypes.object,
-    onProfileEditButtonClick: PropTypes.func.isRequired
-  };
-  user = {
-    imgUrl: '/static/profile.png',
-    data: [
-      {
-        text: 'MIGUEL CARRERA',
-        location: {
-          imgUrl: '/static/prof/Location.svg',
-          text: 'Miami, Fl'
-        }
-      },
-      {
-        text: 'IT developer',
-        location: {
-          imgUrl: '/static/prof/Circle.svg',
-          text: 'Facebook'
-        }
-      },
-      {
-        text: 'IT with positions',
-        location: {
-          imgUrl: '/static/prof/Education.svg',
-          text: 'Hogwards 2007-2011'
-        }
-      }
-    ]
-  };
+  handleRequestCallClick = () =>
+    this.props.onRequestCallClick({
+      professionalId: this.props.user.id
+    });
 
   render() {
+    const {
+      user,
+      loading,
+      error,
+      onProfileEditButtonClick
+    } = this.props;
+
+    if (loading) return <div>Loading</div>;
+    if (error) return <div>{error}</div>;
+
     return (
       <ProfessionalProfile
-        user={this.props.user}
-        onProfileEditButtonClick={this.props.onProfileEditButtonClick}
-      />);
+        user={user}
+        onProfileEditButtonClick={onProfileEditButtonClick}
+        onRequestCallClick={this.handleRequestCallClick}
+        onEditButtonClick={this.props.onEditButtonClick}
+      />
+    );
   }
 }
+
+ProfessionalProfileContainer.propTypes = {
+  id: PropTypes.string.isRequired,
+  onRequestCallClick: PropTypes.func,
+  onEditButtonClick: PropTypes.func,
+  onProfileEditButtonClick: PropTypes.func.isRequired,
+  loading: PropTypes.boolean,
+  error: PropTypes.object,
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    auth0UserId: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    professional: PropTypes.shape({
+      about: PropTypes.string,
+      price: PropTypes.number,
+      location: PropTypes.shape({
+        country: PropTypes.string
+      }),
+      job: PropTypes.shape({
+        company: PropTypes.shape({ name: PropTypes.string.isRequired }).isRequired,
+        jobTitle: PropTypes.shape({ title: PropTypes.string.isRequired }).isRequired
+      }),
+      majors: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        school: PropTypes.shape({
+          university: PropTypes.shape({
+            name: PropTypes.string.isRequired
+          })
+        })
+      }))
+    })
+  })
+};
+
 const getProfessionalById = gql`
-query GetUserById($id: ID!) {
-  allUsers(filter: {id: $id}) {
-    name
-    professional {
-      id
+  query GetUserById($id: ID!) {
+    User(id: $id) {
+      userType
+      auth0UserId
+      name
+      professional {
+        price
+        location {
+          country
+        }
+        job {
+          company { name }
+          jobTitle { title }
+        }
+        majors {
+          name
+          school {
+            university {
+              name
+            }
+          }
+        }
+      }
     }
   }
-}
 `;
 
 export default graphql(getProfessionalById, {
-  options: ({ id }) => console.log('here is the id passed to professionalprofile', id) || ({
-    variables: {
-      id
-    }
-  }),
-  props: ({ data: { allUsers } }) => ({
-    user: allUsers && allUsers[0]
+  options: ({ id }) => ({ variables: { id } }),
+  props: ({ data: { User, error, loading } }) => ({
+    loading, error, user: User
   })
 })(ProfessionalProfileContainer);
