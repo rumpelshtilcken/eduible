50d031b191cba7d979df15e61e3ed72a5672c56c
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-09 01:42:57 +0600

    feat(ProfessionalProfile): connect to graphcool

--- app/components/ProfessionalProfile/index.js
+++ app/components/ProfessionalProfile/index.js
@@ -4,18 +4,50 @@ import ProfessionalProfileHeader from './ProfessionalProfileHeader';
 import Content from './Content';
 import stylesheet from './index.css';
 
-const ProfessionalProfile = ({ user }) => (
+const ProfessionalProfile = ({
+  user,
+  isCurrentUser,
+  onRequestCallClick,
+  onEditButtonClick
+}) => (
   <div className="container">
-    <ProfessionalProfileHeader user={user} />
-    <Content />
+    <ProfessionalProfileHeader
+      user={user}
+      isCurrentUser={isCurrentUser}
+      onRequestCallClick={onRequestCallClick}
+      onEditButtonClick={onEditButtonClick}
+    />
+    <Content user={user} />
     <style jsx>{stylesheet}</style>
   </div>
 );
 
 ProfessionalProfile.propTypes = {
+  onRequestCallClick: PropTypes.func,
+  onEditButtonClick: PropTypes.func,
+  isCurrentUser: PropTypes.bool,
   user: PropTypes.shape({
-    imgUrl: PropTypes.string,
-    data: PropTypes.arrayOf(PropTypes.string).isRequired
+    auth0UserId: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    professional: PropTypes.shape({
+      about: PropTypes.string,
+      price: PropTypes.number,
+      location: PropTypes.shape({
+        country: PropTypes.string
+      }),
+      job: PropTypes.shape({
+        company: PropTypes.shape({ name: PropTypes.string.isRequired }).isRequired,
+        jobTitle: PropTypes.shape({ title: PropTypes.string.isRequired }).isRequired
+      }),
+      majors: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        school: PropTypes.shape({
+          university: PropTypes.shape({
+            name: PropTypes.string.isRequired
+          })
+        })
+      }))
+    })
   })
 };
 
