94a1415db9582137824309c2a71f86ecef95793d
justtairex <justtairex@gmail.com>
2017-10-09 17:45:02 +0600

    fix: validation email

--- app/utils/ValidationUtils.js
+++ app/utils/ValidationUtils.js
@@ -4,9 +4,7 @@ const PASSWORD_MIN_LENGTH = 6;
 
 const ValidationUtils = {
   isValidEmail: candidate =>
-    /^([A-Za-z0-9]{1,})(([.]|[-]|[_]){1}[A-Za-z0-9-+]+){1,}[@]{1}([A-z0-9]+)([.][A-z0-9]+)+$/.test(
-      candidate
-    ),
+  /^([A-Z a-z 0-9]{1,})(([.]|[-]|[_]){0,}[A-Za-z0-9-+]+){0,}[@]{1}([A-z0-9]+)([.][A-z0-9]+){0,}$/.test(candidate),
   isValidPassword: candidate => candidate.length >= PASSWORD_MIN_LENGTH,
   isValidCardNumber: candidate => /^(?:\d[ -]*?){12,18}$/.test(candidate),
   isValidCVV: candidate => /^(?:\d[ -]*?){3}$/.test(candidate),
