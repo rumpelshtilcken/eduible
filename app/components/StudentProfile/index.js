7541afc089e07a864f8b481e96231755bdc9ec7e
ak8912 <kasaselya91@gmail.com>
2017-10-13 17:12:30 +0600

    feat: calendar not fixed

--- app/components/StudentProfile/index.js
+++ app/components/StudentProfile/index.js
@@ -1,10 +1,8 @@
 import { Component } from 'react';
-import { TabMenu } from 'components';
 import PropTypes from 'prop-types';
 
 import Conversations from './TabList/Conversations';
 import Profile from './Profile';
-import Universities from './TabList/Universities';
 
 import stylesheet from './index.css';
 
@@ -44,21 +42,7 @@ class StudentProfile extends Component {
           user={this.props.user}
           onEditButtonClick={this.props.onProfileEditButtonClick}
         />
-        {this.props.universities &&
-        <TabMenu
-          tabs={this.tabs}
-          childrens={[
-            <Universities
-              universities={this.props.universities}
-              onRemoveFromListClick={this.props.onRemoveFromListClick}
-              onReadMoreClick={this.handleReadMoreClick}
-              onCalculatedClick={this.handleCalculatedClick}
-            />,
-            <h1>Section 1 is coming</h1>,
-            <h1>Section 2 is coming</h1>,
-            <Conversations />
-          ]}
-        />}
+        <Conversations />
         <style jsx>{stylesheet}</style>
       </div>
     );
