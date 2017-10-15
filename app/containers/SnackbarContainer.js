f723ba739624c6a3906580bee69c5e16d1e855f0
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-15 18:42:44 +0600

    fix(SnackbarContainer): fix request close

--- app/containers/SnackbarContainer.js
+++ app/containers/SnackbarContainer.js
@@ -1,25 +1,33 @@
+import { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { MuiSnackbar } from 'components';
 import * as snackbarActions from 'actions/snackbar';
 
-const SnackbarContainer = ({ message, messageType, hideSnackbar }) => (
-  <MuiSnackbar
-    isOpen={message && messageType}
-    action="HIDE"
-    message={message}
-    messageType={messageType}
-    handleActionTouchTap={hideSnackbar}
-    handleRequestClose={hideSnackbar}
-  />
-);
+class SnackbarContainer extends Component {
+  static propTypes = {
+    message: PropTypes.string,
+    messageType: PropTypes.string,
+    hideSnackbar: PropTypes.func.isRequired
+  };
 
-SnackbarContainer.propTypes = {
-  message: PropTypes.string,
-  messageType: PropTypes.string,
-  hideSnackbar: PropTypes.func
-};
+  handleRequestClose = () => this.props.hideSnackbar();
+
+  render() {
+    const { message, messageType } = this.props;
+    return (
+      <MuiSnackbar
+        isOpen={!!(message && messageType)}
+        action="HIDE"
+        message={message}
+        messageType={messageType}
+        onActionTouchTap={this.handleRequestClose}
+        onRequestClose={this.handleRequestClose}
+      />
+    );
+  }
+}
 
 const mapStateToProps = state => ({ ...state.snackbar });
 
