8978dd6f17ed09ac8f11e7c287fbfcca24502fe8
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-15 18:41:02 +0600

    feat(StatefulView): fix design

--- app/components/StatefulView/index.js
+++ app/components/StatefulView/index.js
@@ -2,18 +2,15 @@ import PropTypes from 'prop-types';
 
 import Spinner from './Spinner';
 
-const StatefulView = ({ loading, error, children, isRenderErrorView }) => {
-  if (loading) return <Spinner />;
-
-  if (error && isRenderErrorView) return <div>{error}</div>;
-
-  return <div>{children}</div>;
-};
+const StatefulView = ({ loading, children }) => (
+  <div>
+    {loading && <Spinner />}
+    {children}
+  </div>
+);
 
 StatefulView.propTypes = {
   loading: PropTypes.bool,
-  error: PropTypes.string,
-  isRenderErrorView: PropTypes.bool,
   children: PropTypes.element.isRequired
 };
 
