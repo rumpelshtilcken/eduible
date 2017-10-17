18671c0f7dfa85137b86682af58e4196e18fc14e
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-18 01:13:39 +0600

    feat: introduce converter from ISO to string

--- app/utils/auth.js
+++ app/utils/auth.js
@@ -51,3 +51,19 @@ export const convertDateToISO = (date) => {
   const d = new Date(date);
   return d.toISOString();
 };
+
+export const convertFromISOStringFormat = (dateISO) => {
+  const date = new Date(dateISO);
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let dt = date.getDate();
+
+  if (dt < 10) {
+    dt = `0${dt}`;
+  }
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  return `${year}-${month}-${dt}`;
+};
