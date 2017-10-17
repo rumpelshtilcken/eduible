4ddf4b86a23f5616352b138fb97764175fd85222
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-18 03:13:50 +0600

    fix: callback

--- app/config/index.js
+++ app/config/index.js
@@ -7,6 +7,5 @@ export const auth0Config = {
   callbackUrl: 'http://localhost:3000/socialLoginCallback',
   realm: 'Username-Password-Authentication',
   scope: 'openid email  profile',
-  redirectUri: 'http://localhost:3000/socialLoginCallback'
-  // 'http://www.eduible.com/socialLoginCallback'
+  redirectUri: 'http://www.eduible.com/socialLoginCallback'
 };
