54c3136b6e63221b898fbd69faff0dfa6b89e7a7
ak8912 <kasaselya91@gmail.com>
2017-10-18 20:17:31 +0600

    style(StudentProfile/Profile): remaking profile wrapper

--- app/components/StudentProfile/Profile/index.js
+++ app/components/StudentProfile/Profile/index.js
@@ -2,30 +2,32 @@ import PropTypes from 'prop-types';
 
 import { StudentProfileCard as Card } from 'components';
 
-import stylesheet from './index.css';
+import style from './index.css';
 
 const Profile = ({ user, onEditButtonClick }) => (
-  <div className="wrapper">
-    <Card isHalfRound>
-      <div className="content">
-        <div className="userPhoto" />
-        <div className="userInfo">
-          <h2 className="userName">{user.name}</h2>
-          <p className="userLocation">{user.location}</p>
-        </div>
-        <button
-          className="edit"
-          onClick={onEditButtonClick}
-          tabIndex="0"
-        >
-          <img className="editIcon" alt="editIcon " />
-        </button>
-        <div className="edit editButton">
-          <button onClick={onEditButtonClick}>Edit profile</button>
-        </div>
+  <div className="container">
+    <div className="profile">
+      <div className="profileImg">
+        <img src="static/Profile.jpg" alt="profileImage" />
       </div>
-    </Card>
-    <style jsx>{stylesheet}</style>
+      <div className="profileData">
+        <p>{user.name}</p>
+      </div>
+    </div>
+    <div className="btn">
+      <button
+        className="edit"
+        onClick={onEditButtonClick}
+        tabIndex="0"
+      >
+        <img className="editIcon" alt="" />
+      </button>
+      <div>
+        <button className="editButton" onClick={onEditButtonClick}>Edit Profile</button>
+      </div>
+
+    </div>
+    <style jsx>{style}</style>
   </div>
 );
 
