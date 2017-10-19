8e8b27b82e67a9559e5f2979f5fb3e2ff6bc3424
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-19 22:50:08 +0600

    feat(FollowUs): add link to eduible facebook

--- app/components/Footer/followUs/index.js
+++ app/components/Footer/followUs/index.js
@@ -4,7 +4,13 @@ const FollowUs = () => (
   <div className="footerLinksBlock" >
     <div className="title"> FOLLOW US </div>
     <div className="iconContainer" >
-      <a className="icon" key="www.facebook.com" href="https://www.facebook.com"><img src="/static/Icons/Facebook.svg" alt="" /></a>
+      <a
+        className="icon"
+        key="www.facebook.com"
+        href="https://www.facebook.com/Eduible-1862562624009406"
+      >
+        <img src="/static/Icons/Facebook.svg" alt="" />
+      </a>
       <a className="icon" key="www.linkedIn.com" href="https://www.LinkedIn.com"><img src="/static/Icons/LinkedIn.svg" alt="" /></a>
       <a className="icon" key="www.twitter.com" href="https://www.twitter.com"><img src="/static/Icons/Tweeter.svg" alt="" /></a>
     </div>
