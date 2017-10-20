d4029c3556077b07d66370ddb8879e19316db6d3
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-20 20:31:11 +0600

    feat(callRequest): route back

--- app/pages/callRequest.js
+++ app/pages/callRequest.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
+import Router from 'next/router';
 
 import withPage from 'hoc/withPage';
 import CallRequestContainer from 'containers/CallRequestContainer';
@@ -14,18 +15,22 @@ class CallRequest extends Component {
     })
   };
 
-  handleBackButtonClick = () => {
-    window.history.back();
-  };
+  handleBackButtonClick = () => Router.back();
 
   render() {
-    console.log(this.props.url);
+    if (!this.props.url.query && !this.props.url.query.professionalId) return null;
+
+    const { professionalId } = this.props.url.query;
+
     return (
       <div>
         <Head>
           <title>{'Call request'}</title>
         </Head>
-        <CallRequestContainer onBackButtonClick={this.handleBackButtonClick} />
+        <CallRequestContainer
+          professionalId={professionalId}
+          onBackButtonClick={this.handleBackButtonClick}
+        />
       </div>
     );
   }
