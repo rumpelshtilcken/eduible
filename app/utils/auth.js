96e03788274ec94bb7fef42681834fb540193312
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-20 20:31:46 +0600

    feat(auth): convert ISO date to object

--- app/utils/auth.js
+++ app/utils/auth.js
@@ -61,3 +61,19 @@ export const convertFromISOStringFormat = (dateISO) => {
 
   return `${year}-${month}-${dt}`;
 };
+
+export const convertFromISOToObject = (dateISO) => {
+  const date = new Date(dateISO);
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let dt = date.getDate();
+
+  if (dt < 10) {
+    dt = `0${dt}`;
+  }
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  return { year, month, dt };
+};
