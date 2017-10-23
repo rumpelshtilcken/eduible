110002c917c4f3babc82b08e39a8da3ee3db8c1b
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-23 20:22:56 +0600

    feat(SelectDropDownContainer): save value according props

--- app/containers/SelectDropDownContainer.js
+++ app/containers/SelectDropDownContainer.js
@@ -1,12 +1,15 @@
+import { bindActionCreators } from 'redux';
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { SelectDropdown } from 'components';
-import * as searchAction from 'actions/search';
+import * as searchActions from 'actions/search';
+import * as formActions from 'actions/form';
 
 class SelectDropDownContainer extends Component {
   static propTypes = {
+    isFormInput: PropTypes.bool,
     reduxStoreName: PropTypes.string,
     searchUpdate: PropTypes.func.isRequired,
     options: PropTypes.arrayOf(
@@ -17,25 +20,45 @@ class SelectDropDownContainer extends Component {
       }).isRequired
     ).isRequired,
     search: PropTypes.object,
+    form: PropTypes.object,
+    update: PropTypes.func,
+    reset: PropTypes.func,
     resetFilter: PropTypes.func.isRequired
   };
 
-  handleChange = value => (value
+  handleSearchChange = value => (value
     ? this.props.searchUpdate({ name: [this.props.reduxStoreName], value })
     : this.props.resetFilter({ name: [this.props.reduxStoreName] }))
 
+  handleFormChange = value => (value
+    ? this.props.update({ name: [this.props.reduxStoreName], value })
+    : this.props.reset({ name: [this.props.reduxStoreName] }));
+
   render() {
+    const value = this.props.isFormInput
+      ? this.props.form[this.props.reduxStoreName]
+      : this.props.search[this.props.reduxStoreName];
+
+    const handleChange = this.props.isFormInput
+      ? this.handleFormChange
+      : this.handleSearchChange;
+
     return (
       <SelectDropdown
         {...this.props}
         key={this.props.reduxStoreName}
         options={this.props.options}
-        value={this.props.search[this.props.reduxStoreName]}
-        onChange={this.handleChange}
+        value={value}
+        onChange={handleChange}
       />);
   }
 }
 
-const mapStateToProps = ({ search }) => ({ search });
+const mapStateToProps = ({ search, form }) => ({ search, form });
 
-export default connect(mapStateToProps, searchAction)(SelectDropDownContainer);
+const mapDispatchToProps = dispatch => ({
+  ...bindActionCreators(formActions, dispatch),
+  ...bindActionCreators(searchActions, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SelectDropDownContainer);
