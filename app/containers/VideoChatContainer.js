2e236764d36c693129df5bc2dd42e70cb138243b
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-31 16:30:30 +0600

    feat(VideoChatContainer): fetch appointment

--- app/containers/VideoChatContainer.js
+++ app/containers/VideoChatContainer.js
@@ -1,28 +1,69 @@
-/* eslint-disable */
-import React, { Component } from 'react';
+import { graphql, gql, compose } from 'react-apollo';
+import { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import { VideoChat } from 'components';
+import { getCurrentUserData } from 'utils/auth';
+import { StatefulView, VideoChat } from 'components';
 import fetch from 'isomorphic-fetch';
 import withVideoChat from 'hoc/withVideoChat';
 
-// TODO: fetch via graphql
 class VideoChatContainer extends Component {
+  static propTypes = {
+    appointment: PropTypes.shape({
+      state: PropTypes.string,
+      professional: PropTypes.shape({
+        user: PropTypes.shape({
+          auth0UserId: PropTypes.string
+        })
+      }),
+      student: PropTypes.shape({
+        user: PropTypes.shape({
+          auth0UserId: PropTypes.string
+        })
+      })
+    }),
+    appointmentLoading: PropTypes.bool,
+    appointmentError: PropTypes.string
+  };
+
   componentDidMount() {
     this.generateToken();
   }
 
-  user = {
-    name: 'Miguel Carrera',
-    imgUrl: '/static/miguel.jpg',
-    icon: '/static/placeholderIcon.svg',
-    city: 'Miami, Fl',
-    timer: {
-      hour: 0,
-      minutes: 59
+  componentWillReceiveProps(nextProps) {
+    const { appointment, appointmentLoading } = nextProps;
+    if (!appointmentLoading) {
+      const { student, professional } = appointment;
+      const currentUserId = getCurrentUserData('sub');
+
+      if (student.user.auth0UserId !== currentUserId &&
+          professional.user.auth0UserId !== currentUserId) {
+        // TODO: wrong appointmentId
+        return;
+      }
+
+      if (appointment.state !== 'Approve') {
+        // TODO: not approved appointment
+        return;
+      }
+
+      this.handleAppointmentLoad();
     }
+  }
+
+  // getCallId = () => {
+
+  // };
+
+  handleAppointmentLoad = () => {
+    // Appointment loaded
+    // TODO: create call
+    // TODO: create conversation
+    // TODO: generate token
+    // const callId = this.getCallId();
   };
 
-  generateToken = async () => {
+  generateToken = async ({ userName, expiresInSeconds, resourceId }) => {
     try {
       const res = await fetch('/api/v1/videochat', {
         method: 'POST',
@@ -30,30 +71,99 @@ class VideoChatContainer extends Component {
           Accept: 'application/json',
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-          userName: 'John',
-          expiresInSeconds: 5000
-        })
+        body: JSON.stringify({ userName, expiresInSeconds })
       });
 
       const json = await res.json();
-      this.props.onVideoChatParamsLoad({ vidyoToken: 'json.vidyoToken', resourceId: 'Test' });
+      this.props.onVideoChatParamsLoad({ vidyoToken: 'json.vidyoToken', resourceId });
     } catch (error) {
       console.log('Fetch error: ', error);
     }
   };
 
   render() {
+    const { appointmentLoading, appointmentError } = this.props;
+    if (appointmentError) return <div>{appointmentError}</div>;
+
     return (
-      <VideoChat
-        user={this.user}
-        setVideoViewId={this.props.onVideoViewIdLoad}
-        devices={this.props.devices}
-        selectedDevices={this.props.selectedDevices}
-        sendMessageTest={this.props.sendMessageTest}
-      />
+      <StatefulView loading={appointmentLoading}>
+        <VideoChat
+          user={this.user}
+          setVideoViewId={this.props.onVideoViewIdLoad}
+          devices={this.props.devices}
+          selectedDevices={this.props.selectedDevices}
+          sendMessageTest={this.props.sendMessageTest}
+        />
+      </StatefulView>
     );
   }
 }
 
-export default withVideoChat(VideoChatContainer);
+const getAppointment = gql`
+  query Appointment($id: ID!) {
+    Appointment (id: $id) {
+      id
+      state
+      call {
+        id
+      }
+      messages {
+        id
+        message
+      }
+      professional {
+        id
+        user { 
+          id
+          auth0UserId
+          name
+        }
+      }
+      student {
+        id 
+        user {
+          id
+          auth0UserId
+          name
+        }
+      }
+    }
+  }
+`;
+
+const createCall = gql`
+  mutation createCall (
+    $appointmentId: ID!
+    $duration: Float!
+    $callerId: ID!
+    $recevierId: ID!
+  ) {
+    createCall (
+      appointmentId: $appointmentId
+      duration: $duration
+      callerId: $callerId
+      recevierId: $recevierId
+    ) {
+      id
+    }
+  }
+`;
+
+export default withVideoChat(
+  compose(
+    graphql(getAppointment, {
+      name: 'appointment',
+      options: ({ appointmentId }) => ({ variables: { id: appointmentId } }),
+      props: ({ appointment: { Appointment, loading, error } }) => ({
+        appointment: Appointment,
+        appointmentLoading: loading,
+        appointmentError: error
+      })
+    }),
+    graphql(createCall, {
+      props: ({ mutate }) => ({
+        createCall: ({ appointmentId, duration, callerId, recevierId }) =>
+          mutate({ appointmentId, duration, callerId, recevierId })
+      })
+    })
+  ))(VideoChatContainer);
