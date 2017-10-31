3eb0c0cb9fd427a4a05f2e84db0153c6a808af26
adilkhan <adilkhankenzhetaev@gmail.com>
2017-10-31 14:16:38 +0600

    feat(videoChat): take appointmentId from query

--- app/pages/videoChat.js
+++ app/pages/videoChat.js
@@ -1,15 +1,37 @@
+import { Component } from 'react';
 import Head from 'next/head';
+import PropTypes from 'prop-types';
 
 import withPage from 'hoc/withPage';
 import VideoChatContainer from 'containers/VideoChatContainer';
 
-const VideoChatPage = () => (
-  <div>
-    <Head>
-      <title>{'Video chat'}</title>
-    </Head>
-    <VideoChatContainer />
-  </div>
-);
+class VideoChatPage extends Component {
+  static propTypes = {
+    url: PropTypes.shape({
+      query: PropTypes.shape({
+        appointmentId: PropTypes.string.isRequired
+      })
+    })
+  };
+
+  handleDidAppointmentEnd = () => {
+    // TODO: navigate to payment page
+  };
+
+  render() {
+    const { appointmentId } = this.props.url.query;
+
+    return (
+      <div>
+        <Head>
+          <title>{'Video chat'}</title>
+        </Head>
+        <VideoChatContainer
+          appointmentId={appointmentId}
+        />
+      </div>
+    );
+  }
+}
 
 export default withPage(VideoChatPage);
